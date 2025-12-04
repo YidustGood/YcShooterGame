41923bae2ed2d58d69e1f2c82437c836ae44729c
yichen6 <17772337761@163.com>
2025-12-04 22:08:55 +0800

    feat(Equipment): 添加可装备物品片段实现
    - 实现可装备物品功能片段。该片段为物品添加可装备功能，通过其中的EquipmentInst实现装备更多的行为。

---
+++ Plugins/YiChenGameCore/Source/YiChenEquipment/Private/Fragments/InventoryFragment_Equippable.cpp
@@ -0,0 +1,6 @@
+﻿// Copyright (c) 2025 YiChen. All Rights Reserved.
+
+
+#include "Fragments/InventoryFragment_Equippable.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(InventoryFragment_Equippable)
---
+++ Plugins/YiChenGameCore/Source/YiChenEquipment/Public/Fragments/InventoryFragment_Equippable.h
@@ -0,0 +1,22 @@
+﻿// Copyright (c) 2025 YiChen. All Rights Reserved.
+
+#pragma once
+
+#include "YcInventoryItemDefinition.h"
+#include "YiChenEquipment/Public/YcEquipmentDefinition.h"
+#include "InventoryFragment_Equippable.generated.h"
+
+struct FYcEquipmentDefinition;
+
+/**
+ * 可装备功能片段, 给InventoryItem添加了该片段后就拥有了可装备的功能
+ */
+USTRUCT(BlueprintType)
+struct YICHENEQUIPMENT_API FInventoryFragment_Equippable : public FYcInventoryItemFragment
+{
+	GENERATED_BODY()
+	
+	// 装备定义类型
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Equipment")
+	FYcEquipmentDefinition EquipmentDef;
+};

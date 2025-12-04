00f0e367c276bfd8f0096e41913b5d3f468554ce
yichen6 <17772337761@163.com>
2025-12-04 19:17:41 +0800

    feat(Inventory): 添加初始物品状态片段实现
    实现InventoryFragment_InitialItemStats用于管理物品初始状态
    通过GameplayTag和数值键值对配置物品属性
    在物品实例创建时初始化状态并支持通过标签查询

---
+++ Plugins/YiChenGameCore/Source/YiChenInventory/Private/Fragments/InventoryFragment_InitialItemStats.cpp
@@ -0,0 +1,25 @@
+﻿// Copyright (c) 2025 YiChen. All Rights Reserved.
+
+
+#include "Fragments/InventoryFragment_InitialItemStats.h"
+
+#include "GameplayTagContainer.h"
+#include "YcInventoryItemInstance.h"
+
+void FInventoryFragment_InitialItemStats::OnInstanceCreated(UYcInventoryItemInstance* Instance) const
+{
+	for (const auto& Kvp : InitialItemStats)
+	{
+		Instance->AddStatTagStack(Kvp.Key, Kvp.Value);
+	}
+}
+
+int32 FInventoryFragment_InitialItemStats::GetItemStatByTag(FGameplayTag Tag) const
+{
+	if (const int32* StatPtr = InitialItemStats.Find(Tag))
+	{
+		return *StatPtr;
+	}
+
+	return 0;
+}
---
+++ Plugins/YiChenGameCore/Source/YiChenInventory/Public/Fragments/InventoryFragment_InitialItemStats.h
@@ -0,0 +1,25 @@
+﻿// Copyright (c) 2025 YiChen. All Rights Reserved.
+
+#pragma once
+
+#include "GameplayTagContainer.h"
+#include "YcInventoryItemDefinition.h"
+#include "InventoryFragment_InitialItemStats.generated.h"
+
+/**
+ * 初始物品数据状态功能片段, 通过GameplayTag和数值键值对配置物品属性, 在物品实例创建的时候初始化到ItemInst中以实现动态状态和网络复制
+ * 例如：(武器备用子弹Tag,子弹数量)
+ * 注意该片段静态配置然后实例化是设置到ItemInst, 由ItemInst负责动态管理和网络复制
+ */
+USTRUCT(BlueprintType)
+struct YICHENINVENTORY_API FInventoryFragment_InitialItemStats : public FYcInventoryItemFragment
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditDefaultsOnly, Category="Data")
+	TMap<FGameplayTag, int32> InitialItemStats;
+	
+	virtual void OnInstanceCreated(UYcInventoryItemInstance* Instance) const override;
+
+	int32 GetItemStatByTag(FGameplayTag Tag) const;
+};

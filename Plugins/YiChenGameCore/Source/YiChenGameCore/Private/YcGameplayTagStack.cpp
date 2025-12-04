b19cba93b1f3e6fc8e92ea1a4543992464e8132f
yichen6 <17772337761@163.com>
2025-12-04 19:04:32 +0800

    feat(YiChenGameCore): 实现基于FastArray和GameplayTag的数量栈容器
    添加FYcGameplayTagStack和FYcGameplayTagStackContainer结构体，用于管理游戏标签及其堆叠数量
    支持网络同步和快速查询，通过TMap优化查询性能
    包含添加、移除和查询堆叠数量的方法

---
+++ Plugins/YiChenGameCore/Source/YiChenGameCore/Private/YcGameplayTagStack.cpp
@@ -0,0 +1,110 @@
+﻿// Copyright (c) 2025 YiChen. All Rights Reserved.
+
+#include "YcGameplayTagStack.h"
+
+//////////////////////////////////////////////////////////////////////
+// FYcGameplayTagStack
+
+FString FYcGameplayTagStack::GetDebugString() const
+{
+	return FString::Printf(TEXT("%sx%d"), *Tag.ToString(), StackCount);
+}
+
+// FYcGameplayTagStack
+//////////////////////////////////////////////////////////////////////
+
+
+//////////////////////////////////////////////////////////////////////
+// FYcGameplayTagStackContainer
+
+void FYcGameplayTagStackContainer::AddStack(FGameplayTag Tag, int32 StackCount)
+{
+	if (!Tag.IsValid()) // 检查标签有效性
+	{
+		FFrame::KismetExecutionMessage(TEXT("An invalid tag was passed to AddStack"), ELogVerbosity::Warning);
+		return;
+	}
+	if (StackCount <= 0) return; // 检查添加的数量有效性
+
+	for (FYcGameplayTagStack& Stack : Stacks) // 遍历列表查找出对应Tag结构体，并修改值
+	{
+		if (Stack.Tag != Tag) continue;
+
+		const int32 NewCount = Stack.StackCount + StackCount; // 计算新增后的数量
+		Stack.StackCount = NewCount; // 设置新数量
+		TagToCountMap[Tag] = NewCount; // 将新数量同步设置到TMap中，以便查询
+		MarkItemDirty(Stack); // 标记为脏，让新数据可以进行网络同步
+		return;
+	}
+
+	//如果这个Tag还不存在于列表中，那么根据参数在末尾构造一个元素
+	FYcGameplayTagStack& NewStack = Stacks.Emplace_GetRef(Tag, StackCount);
+	MarkItemDirty(NewStack);
+	TagToCountMap.Add(Tag, StackCount); //同步到TMap
+}
+
+void FYcGameplayTagStackContainer::RemoveStack(FGameplayTag Tag, int32 StackCount)
+{
+	if (!Tag.IsValid())
+	{
+		FFrame::KismetExecutionMessage(TEXT("An invalid tag was passed to RemoveStack"), ELogVerbosity::Warning);
+		return;
+	}
+	//@TODO: Should we error if you try to remove a stack that doesn't exist or has a smaller count?
+	// 如果我们尝试删除一个不存在的Stack或者现存StackCount小于要删除的StackCount,我们是否需要抛出错误提示或者通知
+	if (StackCount <= 0) return;
+
+	for (auto It = Stacks.CreateIterator(); It; ++It)
+	{
+		FYcGameplayTagStack& Stack = *It;
+		if (Stack.Tag != Tag) continue;
+
+		if (Stack.StackCount <= StackCount)
+		{
+			// 现存数量<要删除的数量，所以直接把这个Tag从容器中移除
+			It.RemoveCurrent();
+			TagToCountMap.Remove(Tag);	// TMap同步移除
+			MarkArrayDirty();	// 列表长度发生变化需要调用MarkArrayDirty()，才能触发网络同步
+		}
+		else
+		{
+			// 删除指定数量
+			const int32 NewCount = Stack.StackCount - StackCount;	
+			Stack.StackCount = NewCount;
+			TagToCountMap[Tag] = NewCount;
+			MarkItemDirty(Stack);
+		}
+		return;
+	}
+}
+
+void FYcGameplayTagStackContainer::PreReplicatedRemove(const TArrayView<int32> RemovedIndices, int32 FinalSize)
+{
+	// 由于TMap不可复制，所以移除Tag的时候，TMap中映射的Tag需要我们收到预删除通知时手动进行清理，以保证与服务器达成同步，后面的添加修改同理，在收到相应同步后，从Stacks列表中取得数据并同步设置到TMap中
+	for (int32 Index : RemovedIndices)
+	{
+		const FGameplayTag Tag = Stacks[Index].Tag;
+		TagToCountMap.Remove(Tag);
+	}
+}
+
+void FYcGameplayTagStackContainer::PostReplicatedAdd(const TArrayView<int32> AddedIndices, int32 FinalSize)
+{
+	for (int32 Index : AddedIndices)
+	{
+		const FYcGameplayTagStack& Stack = Stacks[Index];
+		TagToCountMap.Add(Stack.Tag, Stack.StackCount);
+	}
+}
+
+void FYcGameplayTagStackContainer::PostReplicatedChange(const TArrayView<int32> ChangedIndices, int32 FinalSize)
+{
+	for (int32 Index : ChangedIndices)
+	{
+		const FYcGameplayTagStack& Stack = Stacks[Index];
+		TagToCountMap[Stack.Tag] = Stack.StackCount;
+	}
+}
+
+// FYcGameplayTagStackContainer
+//////////////////////////////////////////////////////////////////////
---
+++ Plugins/YiChenGameCore/Source/YiChenGameCore/Public/YcGameplayTagStack.h
@@ -0,0 +1,102 @@
+﻿// Copyright (c) 2025 YiChen. All Rights Reserved.
+
+#pragma once
+
+#include "GameplayTagContainer.h"
+#include "Net/Serialization/FastArraySerializer.h"
+#include "YcGameplayTagStack.generated.h"
+
+// 基于GameplayTag的数量栈FastArray实现
+
+struct FYcGameplayTagStackContainer;
+struct FNetDeltaSerializeInfo;
+
+/**
+ * 单条记录的结构体-(GameplayTag,StackCount)
+ */
+USTRUCT(BlueprintType)
+struct YICHENGAMECORE_API FYcGameplayTagStack : public FFastArraySerializerItem
+{
+	GENERATED_BODY()
+
+	FYcGameplayTagStack()
+	{
+	}
+
+	FYcGameplayTagStack(FGameplayTag InTag, int32 InStackCount)
+		: Tag(InTag)
+		  , StackCount(InStackCount)
+	{
+	}
+
+	FString GetDebugString() const;
+
+private:
+	friend FYcGameplayTagStackContainer;
+
+	// 标签
+	UPROPERTY(BlueprintReadOnly, VisibleInstanceOnly, meta=(AllowPrivateAccess = "true"))
+	FGameplayTag Tag;
+
+	// 数量
+	UPROPERTY(BlueprintReadOnly, VisibleInstanceOnly, meta=(AllowPrivateAccess = "true"))
+	int32 StackCount = 0;
+};
+/** FGameplayTagStack的容器 */
+USTRUCT(BlueprintType)
+struct YICHENGAMECORE_API FYcGameplayTagStackContainer : public FFastArraySerializer
+{
+	GENERATED_BODY()
+
+	FYcGameplayTagStackContainer()
+	{
+	}
+	
+public:
+	// 向指定Tag添加堆叠数量(如果StackCount<0，将不执行任何操作)
+	void AddStack(FGameplayTag Tag, int32 StackCount);
+
+	// 从指定Tag移除堆叠数量(如果StackCount<0，将不执行任何操作)
+	void RemoveStack(FGameplayTag Tag, int32 StackCount);
+
+	// 获得指定Tag的堆叠数量 (Tag不存在返回0)
+	int32 GetStackCount(FGameplayTag Tag) const
+	{
+		return TagToCountMap.FindRef(Tag);
+	}
+
+	// 如果指定Tag存在于容器中则返回True
+	bool ContainsTag(FGameplayTag Tag) const
+	{
+		return TagToCountMap.Contains(Tag);
+	}
+
+	//~FFastArraySerializer contract 这三个函数都只会在收到同步的客户端上被调用
+	void PreReplicatedRemove(const TArrayView<int32> RemovedIndices, int32 FinalSize); //客户端收到删除同步，应用同步数据前所调用的函数，用于在删除前进行一些数据处理，参数是即将被删除的元素下标列表
+	void PostReplicatedAdd(const TArrayView<int32> AddedIndices, int32 FinalSize); //客户端收到添加数据同步并完成后调用的函数，参数是新增的元素下表列表
+	void PostReplicatedChange(const TArrayView<int32> ChangedIndices, int32 FinalSize); //客户端收到修改数据同步并完成后调用的函数，参数是发生改变的元素下表列表
+	//~End of FFastArraySerializer contract
+
+	bool NetDeltaSerialize(FNetDeltaSerializeInfo& DeltaParms)
+	{
+		return FFastArraySerializer::FastArrayDeltaSerialize<FYcGameplayTagStack, FYcGameplayTagStackContainer>(Stacks, DeltaParms, *this);
+	}
+
+private:
+	// 网络复制的数量栈数组
+	UPROPERTY(BlueprintReadOnly, VisibleInstanceOnly, meta=(AllowPrivateAccess = "true"))
+	TArray<FYcGameplayTagStack> Stacks;
+	
+	// 映射Stacks，用于加快查询，TMap查询速度>TArray,但是TMap在UE中目前不支持网络复制,所以在FastArray的三个网络复制辅助函数中进行手动操作保证数据一致性
+	UPROPERTY(NotReplicated, BlueprintReadOnly, VisibleInstanceOnly, meta=(AllowPrivateAccess = "true"))
+	TMap<FGameplayTag, int32> TagToCountMap;
+};
+
+template <>
+struct TStructOpsTypeTraits<FYcGameplayTagStackContainer> : public TStructOpsTypeTraitsBase2<FYcGameplayTagStackContainer>
+{
+	enum
+	{
+		WithNetDeltaSerializer = true,
+	};
+};

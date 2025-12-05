0369976d1a78311934ab338e0eac9f1177725e75
yichen6 <17772337761@163.com>
2025-12-05 23:33:19 +0800

    feat(技能系统): 实现技能集合数据资产及授予功能
    添加YcAbilitySet数据资产用于组织和管理游戏技能、效果及属性集的授予
    包含授予句柄管理结构FYcAbilitySet_GrantedHandles用于追踪和移除已授予内容

---
+++ Plugins/YiChenGameCore/Source/YiChenAbility/Private/YcAbilitySet.cpp
@@ -0,0 +1,155 @@
+﻿// Copyright (c) 2025 YiChen. All Rights Reserved.
+
+#include "YcAbilitySet.h"
+#include "YcAbilitySystemComponent.h"
+#include "YiChenAbility.h"
+#include "Abilities/YcGameplayAbility.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(YcAbilitySet)
+
+void FYcAbilitySet_GrantedHandles::AddAbilitySpecHandle(const FGameplayAbilitySpecHandle& Handle)
+{
+	if (Handle.IsValid())
+	{
+		AbilitySpecHandles.Add(Handle);
+	}
+}
+
+void FYcAbilitySet_GrantedHandles::AddGameplayEffectHandle(const FActiveGameplayEffectHandle& Handle)
+{
+	if (Handle.IsValid())
+	{
+		GameplayEffectHandles.Add(Handle);
+	}
+}
+
+void FYcAbilitySet_GrantedHandles::AddAttributeSet(UAttributeSet* Set)
+{
+	GrantedAttributeSets.Add(Set);
+}
+
+void FYcAbilitySet_GrantedHandles::RemoveFromAbilitySystem(UYcAbilitySystemComponent* ASC)
+{
+	check(ASC);
+
+	if (!ASC->IsOwnerActorAuthoritative())
+	{
+		// 必须拥有权威权限才能添加或移除技能集合
+		return;
+	}
+	
+	// 移除所有已授予的游戏技能
+	for (const FGameplayAbilitySpecHandle& Handle : AbilitySpecHandles)
+	{
+		if (Handle.IsValid())
+		{
+			ASC->ClearAbility(Handle);
+		}
+	}
+	
+	// 移除所有已授予的游戏效果
+	for (const FActiveGameplayEffectHandle& Handle : GameplayEffectHandles)
+	{
+		if (Handle.IsValid())
+		{
+			ASC->RemoveActiveGameplayEffect(Handle);
+		}
+	}
+
+	// 移除所有已授予的属性集
+	for (UAttributeSet* Set : GrantedAttributeSets)
+	{
+		ASC->RemoveSpawnedAttribute(Set);
+	}
+	
+	// 清空所有句柄数组
+	AbilitySpecHandles.Reset();
+	GameplayEffectHandles.Reset();
+	GrantedAttributeSets.Reset();
+}
+
+/////////////////////////////////////////////////////////////////////////
+// UYcAbilitySet
+
+UYcAbilitySet::UYcAbilitySet(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
+
+void UYcAbilitySet::GiveToAbilitySystem(UYcAbilitySystemComponent* ASC, FYcAbilitySet_GrantedHandles* OutGrantedHandles, UObject* SourceObject) const
+{
+	check(ASC);
+
+	if (!ASC->IsOwnerActorAuthoritative())
+	{
+		// 必须拥有权威权限才能添加或移除技能集合
+		return;
+	}
+
+	// 授予GA
+	for (int32 AbilityIndex = 0; AbilityIndex < GrantedGameplayAbilities.Num(); ++AbilityIndex)
+	{
+		const FYcAbilitySet_GameplayAbility& AbilityToGrant = GrantedGameplayAbilities[AbilityIndex];
+
+		if (!IsValid(AbilityToGrant.Ability))
+		{
+			UE_LOG(LogYcAbilitySystem, Error, TEXT("GrantedGameplayAbilities[%d] on ability set [%s] is not valid."), AbilityIndex, *GetNameSafe(this));
+			continue;
+		}
+
+		UYcGameplayAbility* AbilityCDO = AbilityToGrant.Ability->GetDefaultObject<UYcGameplayAbility>();
+
+		// 这个GA的特殊副本结构体，包含的技能CDO和技能等级信息（可以理解为一份技能的伪实例，以CDO对象为基础，扩展了结构体中的其他信息，来实现一份技能的实例，因为同一个技能也存在不同的状态，比如不同等级状态的技能）
+		FGameplayAbilitySpec AbilitySpec(AbilityCDO, AbilityToGrant.AbilityLevel);
+		AbilitySpec.SourceObject = SourceObject;
+		// 将InputTag添加到GA的Tags列表中
+		AbilitySpec.GetDynamicSpecSourceTags().AddTag(AbilityToGrant.InputTag);
+
+		const FGameplayAbilitySpecHandle AbilitySpecHandle = ASC->GiveAbility(AbilitySpec);
+		
+		if (OutGrantedHandles)
+		{
+			OutGrantedHandles->AddAbilitySpecHandle(AbilitySpecHandle);
+		}
+	}
+
+	// 授予GE
+	for (int32 EffectIndex = 0; EffectIndex < GrantedGameplayEffects.Num(); ++EffectIndex)
+	{
+		const FYcAbilitySet_GameplayEffect& EffectToGrant = GrantedGameplayEffects[EffectIndex];
+
+		if (!IsValid(EffectToGrant.GameplayEffect))
+		{
+			UE_LOG(LogYcAbilitySystem, Error, TEXT("GrantedGameplayEffects[%d] on ability set [%s] is not valid"), EffectIndex, *GetNameSafe(this));
+			continue;
+		}
+
+		const UGameplayEffect* GameplayEffect = EffectToGrant.GameplayEffect->GetDefaultObject<UGameplayEffect>();
+		const FActiveGameplayEffectHandle GameplayEffectHandle = ASC->ApplyGameplayEffectToSelf(GameplayEffect, EffectToGrant.EffectLevel, ASC->MakeEffectContext());
+
+		if (OutGrantedHandles)
+		{
+			OutGrantedHandles->AddGameplayEffectHandle(GameplayEffectHandle);
+		}
+	}
+
+	// 授予AttributeSet
+	for (int32 SetIndex = 0; SetIndex < GrantedAttributes.Num(); ++SetIndex)
+	{
+		const FYcAbilitySet_AttributeSet& SetToGrant = GrantedAttributes[SetIndex];
+
+		if (!IsValid(SetToGrant.AttributeSet))
+		{
+			UE_LOG(LogYcAbilitySystem, Error, TEXT("GrantedAttributes[%d] on ability set [%s] is not valid"), SetIndex, *GetNameSafe(this));
+			continue;
+		}
+
+		UAttributeSet* NewSet = NewObject<UAttributeSet>(ASC->GetOwner(), SetToGrant.AttributeSet);
+		ASC->AddAttributeSetSubobject(NewSet);
+
+		if (OutGrantedHandles)
+		{
+			OutGrantedHandles->AddAttributeSet(NewSet);
+		}
+	}
+}
---
+++ Plugins/YiChenGameCore/Source/YiChenAbility/Public/YcAbilitySet.h
@@ -0,0 +1,140 @@
+﻿// Copyright (c) 2025 YiChen. All Rights Reserved.
+
+#pragma once
+
+#include "ActiveGameplayEffectHandle.h"
+#include "GameplayAbilitySpecHandle.h"
+#include "GameplayTagContainer.h"
+#include "Engine/DataAsset.h"
+#include "YcAbilitySet.generated.h"
+
+class UYcGameplayAbility;
+class UGameplayEffect;
+class UAttributeSet;
+class UYcAbilitySystemComponent;
+
+/**
+ * 该文件中实现了技能集合定义功能和授予功能, 通过定义技能集合数据资产来组织技能, 然后统一授予目标ASC组件
+ */
+
+/**
+ * 技能集合中用于授予游戏技能的数据, 包含技能类、登记、输入标签
+ */
+USTRUCT(BlueprintType)
+struct FYcAbilitySet_GameplayAbility
+{
+	GENERATED_BODY()
+	
+	// 要授予的游戏技能
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TSubclassOf<UYcGameplayAbility> Ability = nullptr;
+
+	// 授予的技能等级
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	int32 AbilityLevel = 1;
+
+	// 用于处理该技能输入的标签
+	UPROPERTY(EditDefaultsOnly, Meta = (Categories = "InputTag"), BlueprintReadOnly)
+	FGameplayTag InputTag;
+};
+
+/**
+ * 技能集合中用于授予GE的数据
+ */
+USTRUCT(BlueprintType)
+struct FYcAbilitySet_GameplayEffect
+{
+	GENERATED_BODY()
+	
+	// 授予的GE类
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TSubclassOf<UGameplayEffect> GameplayEffect = nullptr;
+	
+	// 授予的GE等级
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	float EffectLevel = 1.0f;
+};
+
+/**
+ * 技能集合中用于授予属性集的数据
+ */
+USTRUCT(BlueprintType)
+struct FYcAbilitySet_AttributeSet
+{
+	GENERATED_BODY()
+	
+	// 要授予的属性集
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TSubclassOf<UAttributeSet> AttributeSet;
+};
+
+/**
+ *	用于存储技能集合已授予内容的句柄数据, 用于追踪管理
+ *	提供对已授予的游戏技能、游戏效果、属性集的句柄管理，以及移除功能
+ */
+USTRUCT(BlueprintType)
+struct YICHENABILITY_API FYcAbilitySet_GrantedHandles
+{
+	GENERATED_BODY()
+	
+	// 添加GA句柄
+	void AddAbilitySpecHandle(const FGameplayAbilitySpecHandle& Handle);
+	// 添加GE句柄
+	void AddGameplayEffectHandle(const FActiveGameplayEffectHandle& Handle);
+	// 添加AttributeSet
+	void AddAttributeSet(UAttributeSet* Set);
+
+	// 从ACS中移除已授予的GA、GE、AttributeSet
+	void RemoveFromAbilitySystem(UYcAbilitySystemComponent* ASC);
+
+protected:
+	// 已授予游戏技能的句柄数组
+	UPROPERTY(BlueprintReadOnly)
+	TArray<FGameplayAbilitySpecHandle> AbilitySpecHandles;
+
+	// 已授予游戏效果的句柄数组
+	UPROPERTY(BlueprintReadOnly)
+	TArray<FActiveGameplayEffectHandle> GameplayEffectHandles;
+
+	// 已授予属性集的指针数组
+	UPROPERTY(BlueprintReadOnly)
+	TArray<TObjectPtr<UAttributeSet>> GrantedAttributeSets;
+};
+
+/**
+ * 技能集合数据资产, 用于定义需要授予的GA、GE、AttributeSet
+ */
+UCLASS(BlueprintType)
+class YICHENABILITY_API UYcAbilitySet : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+public:
+	UYcAbilitySet(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+	
+#if WITH_EDITORONLY_DATA
+	// 此技能集合的本地化描述,仅存在与编辑器状态下方便开发使用
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Description")
+	FText Description = FText::GetEmpty();
+#endif
+	
+	/**
+	 * 将技能集合授予指定的技能系统组件
+	 * 返回的句柄可用于后续移除所有已授予的内容
+	 * @param ASC 目标技能系统组件
+	 * @param OutGrantedHandles 输出的授予句柄
+	 * @param SourceObject 源对象（可选）
+	 */
+	void GiveToAbilitySystem(UYcAbilitySystemComponent* ASC, FYcAbilitySet_GrantedHandles* OutGrantedHandles, UObject* SourceObject = nullptr) const;
+	
+	// 授予此技能集合时要赋予的游戏技能列表
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Gameplay Abilities", meta=(TitleProperty=Ability))
+	TArray<FYcAbilitySet_GameplayAbility> GrantedGameplayAbilities;
+
+	// 授予此技能集合时要赋予的游戏效果列表
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Gameplay Effects", meta=(TitleProperty=GameplayEffect))
+	TArray<FYcAbilitySet_GameplayEffect> GrantedGameplayEffects;
+
+	// 授予此技能集合时要赋予的属性集列表
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Attribute Sets", meta=(TitleProperty=AttributeSet))
+	TArray<FYcAbilitySet_AttributeSet> GrantedAttributes;
+};

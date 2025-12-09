4a6cbd4b9d43deae6cb044a63b0b389fe2915b7e
yichen6 <17772337761@163.com>
2025-12-09 15:26:40 +0800

    feat(Gameplay): 添加等待游戏体验准备完成的蓝图异步操作
    添加AsyncAction_ExperienceReady的.h和.cpp基础文件，用于实现等待异步功能

---
+++ Plugins/YiChenGameCore/Source/YiChenGameplay/Private/GameModes/AsyncAction_ExperienceReady.cpp
@@ -0,0 +1,92 @@
+﻿// Copyright (c) 2025 YiChen. All Rights Reserved.
+
+
+#include "GameModes/AsyncAction_ExperienceReady.h"
+
+#include "GameModes/YcExperienceManagerComponent.h"
+
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(AsyncAction_ExperienceReady)
+
+UAsyncAction_ExperienceReady::UAsyncAction_ExperienceReady(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
+
+UAsyncAction_ExperienceReady* UAsyncAction_ExperienceReady::WaitForExperienceReady(UObject* InWorldContextObject)
+{
+	UAsyncAction_ExperienceReady* Action = nullptr;
+
+	if (UWorld* World = GEngine->GetWorldFromContextObject(InWorldContextObject, EGetWorldErrorMode::LogAndReturnNull))
+	{
+		Action = NewObject<UAsyncAction_ExperienceReady>();
+		Action->WorldPtr = World;
+		// 将异步操作注册到游戏实例，防止对象在异步操作完成前被垃圾回收
+		Action->RegisterWithGameInstance(World);
+	}
+
+	return Action;
+}
+
+void UAsyncAction_ExperienceReady::Activate()
+{
+	UWorld* World = WorldPtr.Get();
+	// 没有世界对象, 永远不会自然完成, 直接结束
+	if (!World) SetReadyToDestroy();
+	
+	// GameState有效就直接通过GameState监听游戏体验加载完成事件, 无效就监听GameState设置事件以获取GameState后再监听
+	if (AGameStateBase* GameState = World->GetGameState())
+	{
+		Step2_ListenToExperienceLoading(GameState);
+	}
+	else
+	{
+		World->GameStateSetEvent.AddUObject(this, &ThisClass::Step1_HandleGameStateSet);
+	}
+}
+
+void UAsyncAction_ExperienceReady::Step1_HandleGameStateSet(AGameStateBase* GameState)
+{
+	if (UWorld* World = WorldPtr.Get())
+	{
+		World->GameStateSetEvent.RemoveAll(this);
+	}
+
+	Step2_ListenToExperienceLoading(GameState);
+}
+
+void UAsyncAction_ExperienceReady::Step2_ListenToExperienceLoading(const AGameStateBase* GameState)
+{
+	check(GameState);
+	UYcExperienceManagerComponent* ExperienceComponent = GameState->FindComponentByClass<UYcExperienceManagerComponent>();
+	check(ExperienceComponent);
+
+	// 已加载就绑定到下一帧调用, 未加载就注册监听加载完成事件
+	if (ExperienceComponent->IsExperienceLoaded())
+	{
+		UWorld* World = GameState->GetWorld();
+		check(World);
+
+		// 体验恰巧已经加载完成，但仍延迟一帧执行，
+		// 以确保开发者不会编写依赖于"体验总是已加载"这种假设的代码
+		// @TODO: 考虑是否为动态生成的内容/加载界面消失后的任何时机取消延迟？
+		// @TODO: 也许在体验加载本身中注入0-1秒的随机延迟？
+		World->GetTimerManager().SetTimerForNextTick(FTimerDelegate::CreateUObject(this, &ThisClass::Step4_BroadcastReady));
+	}
+	else
+	{
+		ExperienceComponent->CallOrRegister_OnExperienceLoaded(FOnYcExperienceLoaded::FDelegate::CreateUObject(this, &ThisClass::Step3_HandleExperienceLoaded));
+	}
+}
+
+void UAsyncAction_ExperienceReady::Step3_HandleExperienceLoaded(const UYcExperienceDefinition* CurrentExperience)
+{
+	Step4_BroadcastReady();
+}
+
+void UAsyncAction_ExperienceReady::Step4_BroadcastReady()
+{
+	OnReady.Broadcast();
+
+	SetReadyToDestroy();
+}
---
+++ Plugins/YiChenGameCore/Source/YiChenGameplay/Public/GameModes/AsyncAction_ExperienceReady.h
@@ -0,0 +1,39 @@
+﻿// Copyright (c) 2025 YiChen. All Rights Reserved.
+
+#pragma once
+
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "AsyncAction_ExperienceReady.generated.h"
+
+class UYcExperienceDefinition;
+
+
+
+/**
+ * 异步等待游戏体验就绪并有效，然后调用 OnReady 事件。如果游戏状态已有效，将立即调用 OnReady。
+ * 适用于等待到游戏体验加载完成后进行某些操作，例如依赖游戏体验的某些资产那必须等到加载完成后进行操作才是安全的
+ */
+UCLASS()
+class YICHENGAMEPLAY_API UAsyncAction_ExperienceReady : public UBlueprintAsyncActionBase
+{
+	GENERATED_UCLASS_BODY()
+public:
+	// 等待Experience被确定并加载
+	UFUNCTION(BlueprintCallable, meta=(WorldContext = "WorldContextObject", BlueprintInternalUseOnly="true"))
+	static UAsyncAction_ExperienceReady* WaitForExperienceReady(UObject* WorldContextObject);
+
+	virtual void Activate() override;
+	
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE(FExperienceReadyAsyncDelegate);
+	// 当Experience被确定并已就绪/加载时调用
+	UPROPERTY(BlueprintAssignable)
+	FExperienceReadyAsyncDelegate OnReady;
+
+private:
+	void Step1_HandleGameStateSet(AGameStateBase* GameState);
+	void Step2_ListenToExperienceLoading(const AGameStateBase* GameState);
+	void Step3_HandleExperienceLoaded(const UYcExperienceDefinition* CurrentExperience);
+	void Step4_BroadcastReady();
+
+	TWeakObjectPtr<UWorld> WorldPtr;
+};

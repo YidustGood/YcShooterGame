055e15c44a8626d6628e6feb0dae4c5cd4c364c7
yichen6 <17772337761@163.com>
2025-12-15 23:38:52 +0800

    feat(红点系统): 添加GameplayTag配置支持
    在模块启动时添加对游戏标签配置文件的搜索路径，用于支持红点系统的GameplayTag配置文件

--- Plugins/YiChenRedDotSystem/Source/YiChenRedDotSystem/Private/YiChenRedDotSystem.cpp
+++ Plugins/YiChenRedDotSystem/Source/YiChenRedDotSystem/Private/YiChenRedDotSystem.cpp
@@ -2,12 +2,15 @@
 
 #include "YiChenRedDotSystem.h"
 
+#include "GameplayTagsManager.h"
+
 #define LOCTEXT_NAMESPACE "FYiChenRedDotSystemModule"
 DEFINE_LOG_CATEGORY(LogYcRedDot);
 
 void FYiChenRedDotSystemModule::StartupModule()
 {
 	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+	UGameplayTagsManager::Get().AddTagIniSearchPath(FPaths::ProjectPluginsDir() / TEXT("YiChenRedDotSystem/Config/Tags"));
 }
 
 void FYiChenRedDotSystemModule::ShutdownModule()

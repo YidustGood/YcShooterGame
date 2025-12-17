f2cedb248688178161f7eff34411fe16740d8914
yichen6 <17772337761@163.com>
2025-12-17 23:33:35 +0800

    feat(LocalPlayer): 添加YcLocalPlayer基础类实现
    实现本地玩家基础类，用于处理游戏设置相关内容。需要在项目设置中指定使用此类或其子类作为本地玩家类。

---
+++ Plugins/YiChenGameCore/Source/YiChenGameplay/Private/Player/YcLocalPlayer.cpp
@@ -0,0 +1,7 @@
+﻿// Copyright (c) 2025 YiChen. All Rights Reserved.
+
+
+#include "Player/YcLocalPlayer.h"
+
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(YcLocalPlayer)
---
+++ Plugins/YiChenGameCore/Source/YiChenGameplay/Public/Player/YcLocalPlayer.h
@@ -0,0 +1,17 @@
+﻿// Copyright (c) 2025 YiChen. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonLocalPlayer.h"
+#include "YcLocalPlayer.generated.h"
+
+/**
+ * 插件所提供的LocalPlayer类, 需要在项目设置->引擎-一般设置->默认类-本地玩家类中指定使用YcLocalPlayer或其子类
+ * 游戏设置相关内容可以通过LocalPlayer进行部分实现
+ */
+UCLASS()
+class YICHENGAMEPLAY_API UYcLocalPlayer : public UCommonLocalPlayer
+{
+	GENERATED_BODY()
+};

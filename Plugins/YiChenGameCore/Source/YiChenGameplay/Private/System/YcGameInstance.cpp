cb5431240dd0f9ab8ba6fca545f9a011838b2b7d
yichen6 <17772337761@163.com>
2025-12-17 23:30:45 +0800

    feat(GameInstance): 添加YcGameInstance基础类实现
    实现游戏实例基类，继承自UCommonGameInstance，提供全局状态管理和核心系统访问
    - 添加主玩家控制器访问方法
    - 实现初始化生命周期管理
    - 注册自定义初始化状态链条

---
+++ Plugins/YiChenGameCore/Source/YiChenGameplay/Private/System/YcGameInstance.cpp
@@ -0,0 +1,38 @@
+﻿// Copyright (c) 2025 YiChen. All Rights Reserved.
+
+
+#include "System/YcGameInstance.h"
+
+#include "YcGameplayTags.h"
+#include "Components/GameFrameworkComponentManager.h"
+#include "Player/YcPlayerController.h"
+
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(YcGameInstance)
+
+AYcPlayerController* UYcGameInstance::GetPrimaryPlayerController() const
+{
+	return Cast<AYcPlayerController>(Super::GetPrimaryPlayerController(false));
+}
+
+void UYcGameInstance::Init()
+{
+	Super::Init();
+	
+	// 注册我们自定义的初始状态链条
+	UGameFrameworkComponentManager* ComponentManager = GetSubsystem<UGameFrameworkComponentManager>(this);
+
+	if (ensure(ComponentManager))
+	{
+		ComponentManager->RegisterInitState(YcGameplayTags::InitState_Spawned, false, FGameplayTag());
+		ComponentManager->RegisterInitState(YcGameplayTags::InitState_DataAvailable, false, YcGameplayTags::InitState_Spawned);
+		ComponentManager->RegisterInitState(YcGameplayTags::InitState_DataInitialized, false, YcGameplayTags::InitState_DataAvailable);
+		ComponentManager->RegisterInitState(YcGameplayTags::InitState_GameplayReady, false, YcGameplayTags::InitState_DataInitialized);
+	}
+}
+
+void UYcGameInstance::Shutdown()
+{
+	Super::Shutdown();
+	
+}
---
+++ Plugins/YiChenGameCore/Source/YiChenGameplay/Public/System/YcGameInstance.h
@@ -0,0 +1,39 @@
+﻿// Copyright (c) 2025 YiChen. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonGameInstance.h"
+#include "YcGameInstance.generated.h"
+
+class AYcPlayerController;
+
+/**
+ * 插件的游戏实例基类，提供游戏全局状态管理和核心系统访问
+ * 
+ * 该游戏实例类继承自UCommonGameInstance，是YiChen游戏框架的核心组件之一。
+ * 负责管理游戏全局状态、系统初始化和游戏流程控制。
+ * 
+ * 主要功能：
+ * - 提供对主玩家控制器的便捷访问
+ * - 管理游戏生命周期（初始化、关闭）
+ * - 作为全局系统和子系统的访问入口
+ * 
+ * 使用要求：
+ * - 使用YiChenCore插件的项目必须使用YcGameInstance或其子类作为游戏实例
+ * - 项目设置中需设置游戏视口客户端类为CommonGameViewportClient或其子类
+   - 项目设置中需设置本地玩家类为YcLocalPlayer或其子类(继承自CommonLocalPlayer)
+ * - 可以在项目设置中配置为游戏实例类
+ */
+UCLASS(Config = Game)
+class YICHENGAMEPLAY_API UYcGameInstance : public UCommonGameInstance
+{
+	GENERATED_BODY()
+
+public:
+	AYcPlayerController* GetPrimaryPlayerController() const;
+	
+protected:
+    virtual void Init() override;
+    virtual void Shutdown() override;
+};

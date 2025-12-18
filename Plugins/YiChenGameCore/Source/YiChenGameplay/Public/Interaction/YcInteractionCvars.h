b38b1abfd8f0d5d8a74226eb05b1e603a778b4dc
yichen6 <17772337761@163.com>
2025-12-18 23:59:39 +0800

    feat(交互系统): 添加绘制调试形状的控制台变量
    添加 CVarDrawDebugShape 控制台变量用于控制是否绘制交互对象查询范围的调试形状，便于开发调试

---
+++ Plugins/YiChenGameCore/Source/YiChenGameplay/Public/Interaction/YcInteractionCvars.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+namespace YcConsoleVariables
+{
+	// 控制是否绘制交互对象查询范围。 使用方法: 1.引入本头文件 2.YcConsoleVariables::CVarDrawDebugShape.GetValueOnGameThread()获取值
+	static TAutoConsoleVariable<bool> CVarDrawDebugShape(
+		TEXT("Yc.Interact.DrawDebugShape"),
+		false,
+		TEXT("是否绘制交互系统的调试形状."),
+		ECVF_Default);
+}

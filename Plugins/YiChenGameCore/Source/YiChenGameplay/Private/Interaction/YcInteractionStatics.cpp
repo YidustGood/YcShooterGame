f8ac54fb5f90180c602179a9a53148d1571e41b8
yichen6 <17772337761@163.com>
2025-12-19 00:02:57 +0800

    feat(交互系统): 添加交互系统静态工具函数库
    实现交互系统的蓝图函数库，提供获取交互目标所属Actor、从Actor/重叠结果/命中结果中提取交互目标等功能

---
+++ Plugins/YiChenGameCore/Source/YiChenGameplay/Private/Interaction/YcInteractionStatics.cpp
@@ -0,0 +1,106 @@
+﻿// Copyright (c) 2025 YiChen. All Rights Reserved.
+
+
+#include "Interaction/YcInteractionStatics.h"
+
+#include "Engine/OverlapResult.h"
+#include "Interaction/YcInteractableTarget.h"
+
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(YcInteractionStatics)
+
+UYcInteractionStatics::UYcInteractionStatics(): Super(FObjectInitializer::Get())
+{
+}
+
+AActor* UYcInteractionStatics::GetActorFromInteractableTarget(TScriptInterface<IYcInteractableTarget> InteractableTarget)
+{
+	UObject* Object = InteractableTarget.GetObject();
+	if (Object == nullptr) return nullptr;
+	
+	if (AActor* Actor = Cast<AActor>(Object))
+	{
+		return Actor;
+	}
+	else if (UActorComponent* ActorComponent = Cast<UActorComponent>(Object))
+	{
+		return ActorComponent->GetOwner();
+	}
+	else
+	{
+		unimplemented();
+	}
+	
+	return nullptr;
+}
+
+
+void UYcInteractionStatics::GetInteractableTargetsFromActor(AActor* Actor, TArray<TScriptInterface<IYcInteractableTarget>>& OutInteractableTargets)
+{
+	// 首先，检查Actor本身是否直接实现了交互接口。
+	TScriptInterface<IYcInteractableTarget> InteractableActor(Actor);
+	if (InteractableActor)
+	{
+		OutInteractableTargets.Add(InteractableActor);
+	}
+	
+	// 其次，查找并添加所有实现了交互接口的组件。
+	TArray<UActorComponent*> InteractableComponents = Actor ? Actor->GetComponentsByInterface(UYcInteractableTarget::StaticClass()) : TArray<UActorComponent*>();
+	for (UActorComponent* InteractableComponent : InteractableComponents)
+	{
+		OutInteractableTargets.Add(TScriptInterface<IYcInteractableTarget>(InteractableComponent));
+	}
+}
+
+void UYcInteractionStatics::AppendInteractableTargetsFromOverlapResults(const TArray<FOverlapResult>& OverlapResults, TArray<TScriptInterface<IYcInteractableTarget>>& OutInteractableTargets)
+{
+	for (const FOverlapResult& Overlap : OverlapResults)
+	{
+		// 检查重叠的Actor是否实现了交互接口。
+		TScriptInterface<IYcInteractableTarget> InteractableActor(Overlap.GetActor());
+		if (InteractableActor)
+		{
+			OutInteractableTargets.AddUnique(InteractableActor);
+		}
+		
+		// 检查重叠的Actor中是否拥有实现了交互接口的组件。
+		// 这通常用于查找如UYcInteractableComponent这样的组件。
+		TScriptInterface<IYcInteractableTarget> InteractableComponent(Overlap.GetActor()->FindComponentByInterface(UYcInteractableTarget::StaticClass()));
+		if (InteractableComponent)
+		{
+			OutInteractableTargets.AddUnique(InteractableComponent);
+		}
+		
+		// 注释掉的代码：直接从OverlapResult中获取组件。当前逻辑不需要，因为它可能不是我们期望的交互组件。
+		// TScriptInterface<IYcInteractableTarget> InteractableComponent(Overlap.GetComponent());
+		// if (InteractableComponent)
+		// {
+		// 	OutInteractableTargets.AddUnique(InteractableComponent);
+		// }
+	}
+}
+
+void UYcInteractionStatics::GetInteractableTargetsFromHitResult(const FHitResult& HitResult, TArray<TScriptInterface<IYcInteractableTarget>>& OutInteractableTargets)
+{
+	// 检查命中的Actor是否实现了交互接口。
+	TScriptInterface<IYcInteractableTarget> InteractableActor(HitResult.GetActor());
+	if (InteractableActor)
+	{
+		OutInteractableTargets.AddUnique(InteractableActor);
+	}
+	
+	// 检查命中的Actor中是否拥有实现了交互接口的组件。
+	// 这通常用于查找如UYcInteractableComponent这样的组件。
+	TScriptInterface<IYcInteractableTarget> InteractableComponent(HitResult.GetActor()->FindComponentByInterface(UYcInteractableTarget::StaticClass()));
+	if (InteractableComponent)
+	{
+		OutInteractableTargets.AddUnique(InteractableComponent);
+	}
+	
+	// 注释掉的代码：直接从HitResult中获取组件。当前逻辑不需要，因为它可能不是我们期望的交互组件。
+	// const TScriptInterface<IYcInteractableTarget> InteractableComponent(HitResult.GetComponent());
+	// if (InteractableComponent)
+	// {
+	// 	OutInteractableTargets.AddUnique(InteractableComponent);
+	// }
+}
---
+++ Plugins/YiChenGameCore/Source/YiChenGameplay/Public/Interaction/YcInteractionStatics.h
@@ -0,0 +1,52 @@
+﻿// Copyright (c) 2025 YiChen. All Rights Reserved.
+
+#pragma once
+
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "YcInteractionStatics.generated.h"
+
+class IYcInteractableTarget;
+template <typename InterfaceType> class TScriptInterface;
+
+/**
+ * @class UYcInteractionStatics
+ * @brief 交互系统的蓝图函数库，提供了一系列用于处理交互目标的静态工具函数。
+ */
+UCLASS()
+class YICHENGAMEPLAY_API UYcInteractionStatics : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+public:
+	UYcInteractionStatics();
+	
+	/**
+	 * 从一个可交互目标接口获取其所属的Actor。
+	 * @param InteractableTarget 实现了IYcInteractableTarget接口的对象。
+	 * @return 如果目标是Actor或Actor组件，则返回其所属的Actor；否则返回nullptr。
+	 */
+	UFUNCTION(BlueprintCallable, Category = "Interaction")
+	static AActor* GetActorFromInteractableTarget(TScriptInterface<IYcInteractableTarget> InteractableTarget);
+
+	/**
+	 * 从给定的Actor上获取所有实现了IYcInteractableTarget接口的对象。
+	 * 这包括Actor本身以及其拥有的所有组件。
+	 * @param Actor 要从中查找交互目标的Actor。
+	 * @param OutInteractableTargets 用于存储找到的交互目标接口的数组。
+	 */
+	UFUNCTION(BlueprintCallable, Category = "Interaction")
+	static void GetInteractableTargetsFromActor(AActor* Actor, TArray<TScriptInterface<IYcInteractableTarget>>& OutInteractableTargets);
+
+	/**
+	 * 从一组重叠检测结果中提取并追加所有可交互目标。
+	 * @param OverlapResults 物理重叠查询的结果数组。
+	 * @param OutInteractableTargets 用于追加找到的可交互目标的数组。
+	 */
+	static void AppendInteractableTargetsFromOverlapResults(const TArray<FOverlapResult>& OverlapResults, TArray<TScriptInterface<IYcInteractableTarget>>& OutInteractableTargets);
+	
+	/**
+	 * 从一个命中检测结果中提取所有可交互目标。
+	 * @param HitResult 物理射线或形状检测的命中结果。
+	 * @param OutInteractableTargets 用于存储找到的可交互目标的数组。
+	 */
+	static void GetInteractableTargetsFromHitResult(const FHitResult& HitResult, TArray<TScriptInterface<IYcInteractableTarget>>& OutInteractableTargets);
+};

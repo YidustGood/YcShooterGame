7a4cc7c7631e6b83a7171067c85ebe35b1d88648
yichen6 <17772337761@163.com>
2025-12-19 20:47:49 +0800

    build(YiChenGameplay): 添加GameplayTasks模块依赖
    添加GameplayTasks模块以支持游戏任务功能

--- Plugins/YiChenGameCore/Source/YiChenGameplay/YiChenGameplay.Build.cs
+++ Plugins/YiChenGameCore/Source/YiChenGameplay/YiChenGameplay.Build.cs
@@ -35,6 +35,7 @@ public class YiChenGameplay : ModuleRules
                 "CommonUser",
                 "EngineSettings",
                 "UMG",
+                "GameplayTasks"
             }
         );
     }

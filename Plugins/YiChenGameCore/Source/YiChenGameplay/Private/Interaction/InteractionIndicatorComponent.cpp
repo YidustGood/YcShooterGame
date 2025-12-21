ef636d9e9c4b8c6b8d9deafe3bd74771780bd06c
yichen6 <17772337761@163.com>
2025-12-21 13:41:02 +0800

    feat(交互系统): 添加交互指示器组件用于管理玩家可交互对象
    实现交互指示器组件(UInteractionIndicatorComponent)及其描述结构(FIndicatorDescriptor)，用于存储和管理玩家当前可交互的对象数据。该组件挂载在玩家控制器上，提供添加、获取和清空交互指示器的功能，支持通过蓝图调用。

---
+++ Plugins/YiChenGameCore/Source/YiChenGameplay/Private/Interaction/InteractionIndicatorComponent.cpp
@@ -0,0 +1,50 @@
+﻿// Copyright (c) 2025 YiChen. All Rights Reserved.
+
+
+#include "Interaction/InteractionIndicatorComponent.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(InteractionIndicatorComponent)
+
+void FIndicatorDescriptor::SetIndicatorManagerComponent(UInteractionIndicatorComponent* InManager)
+{
+	// 确保没有设置过
+	if (ensure(ManagerPtr.IsExplicitlyNull()))
+	{
+		ManagerPtr = InManager;
+	}
+}
+
+UInteractionIndicatorComponent::UInteractionIndicatorComponent(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	bAutoRegister = true;
+	bAutoActivate = true;
+}
+
+UInteractionIndicatorComponent* UInteractionIndicatorComponent::GetComponent(const AController* Controller)
+{
+	if (Controller)
+	{
+		return Controller->FindComponentByClass<UInteractionIndicatorComponent>();
+	}
+
+	return nullptr;
+}
+
+void UInteractionIndicatorComponent::AddIndicator(FIndicatorDescriptor IndicatorDescriptor)
+{
+	auto& Ref = Indicators.Add_GetRef(IndicatorDescriptor);
+	Ref.SetIndicatorManagerComponent(this);
+}
+
+bool UInteractionIndicatorComponent::GetFirstIndicator(FIndicatorDescriptor& InOutIndicator)
+{
+	if (Indicators.IsEmpty()) return false;
+	InOutIndicator = Indicators[0];
+	return true;
+}
+
+void UInteractionIndicatorComponent::EmptyIndicators()
+{
+	Indicators.Empty();
+}
---
+++ Plugins/YiChenGameCore/Source/YiChenGameplay/Public/Interaction/InteractionIndicatorComponent.h
@@ -0,0 +1,69 @@
+﻿// Copyright (c) 2025 YiChen. All Rights Reserved.
+
+#pragma once
+
+#include "Components/ControllerComponent.h"
+#include "InteractionIndicatorComponent.generated.h"
+
+class UInteractionIndicatorComponent;
+class IYcInteractableTarget;
+
+/**
+ * 指示器包装结构
+ * 用于包装玩家可交互对象数据, 在需要交互的时候取出使用
+ */
+USTRUCT(BlueprintType)
+struct FIndicatorDescriptor
+{
+	GENERATED_BODY()
+	
+public:	
+	/** 指向的Actor类型可交互对象 */
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<AActor> TargetActor;
+	
+	/** 交互对象的接口实例 */
+	UPROPERTY(BlueprintReadOnly)
+	TScriptInterface<IYcInteractableTarget> InteractableTarget;
+	
+	UInteractionIndicatorComponent* GetIndicatorManagerComponent() const { return ManagerPtr.Get(); }
+	void SetIndicatorManagerComponent(UInteractionIndicatorComponent* InManager);
+	
+private:
+	/** 所属的交互指示器组件, 通过这个所属组件可以获得相关的玩家角色等对象 */
+	UPROPERTY()
+	TWeakObjectPtr<UInteractionIndicatorComponent> ManagerPtr; 
+};
+
+/**
+ * 交互指示器组件, 存储管理玩家当前可交互的对象数据
+ * 是挂载在玩家控制器上的
+ */
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class YICHENGAMEPLAY_API UInteractionIndicatorComponent : public UControllerComponent
+{
+	GENERATED_BODY()
+public:
+	UInteractionIndicatorComponent(const FObjectInitializer& ObjectInitializer);
+
+	static UInteractionIndicatorComponent* GetComponent(const AController* Controller);
+
+	/** 添加指示器 */
+	UFUNCTION(BlueprintCallable, Category = Indicator)
+	void AddIndicator(FIndicatorDescriptor IndicatorDescriptor);
+	
+	/** 获取首个指示器, 适用于获取玩家视野聚焦的交互物体数据 */
+	UFUNCTION(BlueprintCallable, Category = Indicator)
+	bool GetFirstIndicator(FIndicatorDescriptor& InOutIndicator);
+	
+	/** 清空所有指示器 */
+	UFUNCTION(BlueprintCallable, Category = Indicator)
+	void EmptyIndicators();
+	
+	const TArray<FIndicatorDescriptor>& GetIndicators() const { return Indicators; }
+
+private:
+	/** 存储玩家当前可进行交互的对象数据列表 */
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly , meta=(AllowPrivateAccess = "true"))
+	TArray<FIndicatorDescriptor> Indicators;
+};

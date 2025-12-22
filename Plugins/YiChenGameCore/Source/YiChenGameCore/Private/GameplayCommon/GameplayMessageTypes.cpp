c62e92a90f60a759b3ab4bac60817aa31d73273c
yichen6 <17772337761@163.com>
2025-12-22 21:24:06 +0800

    feat(YiChenGameCore): 添加玩家初始化完成事件消息类型
    实现跨模块低耦合的玩家加入事件响应机制，包含消息标签定义和消息结构体

---
+++ Plugins/YiChenGameCore/Source/YiChenGameCore/Private/GameplayCommon/GameplayMessageTypes.cpp
@@ -0,0 +1,12 @@
+﻿// Copyright (c) 2025 YiChen. All Rights Reserved.
+
+
+#include "GameplayCommon/GameplayMessageTypes.h"
+
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(GameplayMessageTypes)
+
+namespace YcGameplayTags
+{
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Gameplay_GameMode_PlayerInitialized, "Gameplay_GameMode_PlayerInitialized", "玩家登入游戏Controller初始化完成事件消息通道");
+}
---
+++ Plugins/YiChenGameCore/Source/YiChenGameCore/Public/GameplayCommon/GameplayMessageTypes.h
@@ -0,0 +1,33 @@
+﻿// Copyright (c) 2025 YiChen. All Rights Reserved.
+
+#pragma once
+
+#include "NativeGameplayTags.h"
+#include "GameplayMessageTypes.generated.h"
+
+namespace YcGameplayTags
+{
+	YICHENGAMECORE_API	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Gameplay_GameMode_PlayerInitialized);
+}
+
+/**
+ * 玩家登录后事件消息, 当玩家或机器人加入游戏时触, 以及在无缝和非无缝旅行后触发, 此时Controller已经初始化完成了
+ * 配合GameplayMessageSubsystem广播消息, 实现跨模块低耦合的响应玩家加入事件
+ * 是从GameMode的GenericPlayerInitialization()中广播消息, 由PostLogin()和HandleSeamlessTravelPlayer()发起的
+ */
+USTRUCT(BlueprintType)
+struct FGameModePlayerInitializedMessage
+{
+	GENERATED_BODY()
+	FGameModePlayerInitializedMessage() = default;
+	FGameModePlayerInitializedMessage(const TObjectPtr<AGameModeBase> InGameMode, const TObjectPtr<AController> InNewPlayer):
+	GameMode(InGameMode), NewPlayer(InNewPlayer){}
+	
+	/** GameMode对象 */
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<AGameModeBase> GameMode;
+	
+	/** 新的控制器, 可能是AI也可能是真实玩家 */
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<AController> NewPlayer;
+};

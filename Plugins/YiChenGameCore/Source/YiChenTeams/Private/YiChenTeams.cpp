f759d106ca1fa76698c61f406d2005dee442b92d
yichen6 <17772337761@163.com>
2025-12-22 13:40:07 +0800

    feat(团队系统): 添加YiChenTeams模块基础框架
    添加YiChenTeams模块的头文件、实现文件和构建配置，为团队系统功能开发提供基础框架

---
+++ Plugins/YiChenGameCore/Source/YiChenTeams/Private/YiChenTeams.cpp
@@ -0,0 +1,18 @@
+﻿#include "YiChenTeams.h"
+#include "Logging/LogMacros.h"
+#define LOCTEXT_NAMESPACE "FYiChenTeamsModule"
+DEFINE_LOG_CATEGORY(LogYcTeamSystem);
+
+void FYiChenTeamsModule::StartupModule()
+{
+    
+}
+
+void FYiChenTeamsModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FYiChenTeamsModule, YiChenTeams)
---
+++ Plugins/YiChenGameCore/Source/YiChenTeams/YiChenTeams.Build.cs
@@ -0,0 +1,32 @@
+﻿using UnrealBuildTool;
+
+public class YiChenTeams : ModuleRules
+{
+    public YiChenTeams(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore",
+                "YiChenGameCore",
+                "GameplayTags",
+                "GameplayAbilities",
+                "ModularGameplay", 
+                "GameplayMessageRuntime",
+                "AIModule"
+            }
+        );
+    }
+}

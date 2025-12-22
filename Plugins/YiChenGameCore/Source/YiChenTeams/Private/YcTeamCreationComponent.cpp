3d4781de93928dbe6c52f0b5044c1c416627ffe3
yichen6 <17772337761@163.com>
2025-12-22 15:10:39 +0800

    feat(团队系统): 添加队伍创建及自动分配组件
    实现挂载在GameState上的队伍创建组件，支持配置多支队伍并自动平衡分配玩家
    包含服务器端队伍创建、玩家分配逻辑及团队公开信息生成

---
+++ Plugins/YiChenGameCore/Source/YiChenTeams/Private/YcTeamCreationComponent.cpp
@@ -0,0 +1,148 @@
+﻿// Copyright (c) 2025 YiChen. All Rights Reserved.
+
+
+#include "YcTeamCreationComponent.h"
+
+#include "GenericTeamAgentInterface.h"
+#include "YcTeamAgentInterface.h"
+#include "YcTeamPublicInfo.h"
+#include "YcTeamSubsystem.h"
+#include "YiChenTeams.h"
+#include "GameFramework/GameplayMessageSubsystem.h"
+#include "GameFramework/PlayerState.h"
+#include "GameplayCommon/ExperienceMessageTypes.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(YcTeamCreationComponent)
+
+UYcTeamCreationComponent::UYcTeamCreationComponent(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	// 设置默认的团队公开信息类
+	PublicTeamInfoClass = AYcTeamPublicInfo::StaticClass();
+	// 启用使用注册子对象列表进行网络复制
+	bReplicateUsingRegisteredSubObjectList = true;
+}
+
+#if WITH_EDITOR
+EDataValidationResult UYcTeamCreationComponent::IsDataValid(FDataValidationContext& Context) const
+{
+	EDataValidationResult Result = Super::IsDataValid(Context);
+
+	//@TODO: TEAMS: 验证所有队伍资产是否设置了相同的属性！
+
+	return Result;
+}
+#endif
+
+void UYcTeamCreationComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// 监听经验加载完成事件
+	UGameplayMessageSubsystem& Subsystem = UGameplayMessageSubsystem::Get(this);
+	Subsystem.RegisterListener(YcGameplayTags::Experience_StateEvent_Loaded_HighPriority, this, &ThisClass::OnExperienceLoaded);
+}
+
+void UYcTeamCreationComponent::OnExperienceLoaded(FGameplayTag Channel, const FExperienceLoadedMessage& Message)
+{
+	// 创建队伍的逻辑仅在服务器端执行
+#if WITH_SERVER_CODE
+	if (HasAuthority())
+	{
+		// 先创建所有配置的队伍
+		ServerCreateTeams();
+		// 然后为当前已加入的玩家分配团队
+		ServerAssignPlayersToTeams();
+	}
+#endif
+}
+
+#if WITH_SERVER_CODE
+
+void UYcTeamCreationComponent::ServerCreateTeams()
+{
+	// 遍历配置的团队列表，逐个创建团队
+	for (const auto& KVP : TeamsToCreate)
+	{
+		const int32 TeamId = KVP.Key;
+		ServerCreateTeam(TeamId, KVP.Value);
+	}
+}
+
+void UYcTeamCreationComponent::ServerAssignPlayersToTeams()
+{
+	// 为已存在的玩家分配团队
+	AGameStateBase* GameState = GetGameStateChecked<AGameStateBase>();
+	for (APlayerState* PS : GameState->PlayerArray)
+	{
+		ServerChooseTeamForPlayer(PS);
+	}
+	
+	// @TODO 处理新玩家加入时的队伍分配，实际上就是OnExperienceLoaded之后的新玩家
+}
+
+void UYcTeamCreationComponent::ServerChooseTeamForPlayer(APlayerState* PS)
+{
+	// 检查PlayerState是否实现了团队代理接口
+	TScriptInterface<IYcTeamAgentInterface> TeamAgent(PS);
+	UYcTeamSubsystem::FindTeamAgentFromActor(PS, TeamAgent);
+	if (!TeamAgent)
+	{
+		UE_LOG(LogYcTeamSystem, Warning, TEXT("为玩家选择队伍失败, 请确保PlayerState实现了IYcTeamAgentInterface接口!"));
+		return;
+	}
+	
+	// 如果是观众，设置为无团队
+	if (PS->IsOnlyASpectator())
+	{
+		TeamAgent->SetGenericTeamId(FGenericTeamId::NoTeam);
+	}
+	else
+	{
+		// 为正常玩家分配到人数最少的团队，以保持团队平衡
+		const FGenericTeamId TeamID = IntegerToGenericTeamId(GetLeastPopulatedTeamID());
+		TeamAgent->SetGenericTeamId(TeamID);
+	}
+}
+
+void UYcTeamCreationComponent::ServerCreateTeam(const int32 TeamId, UYcTeamAsset* TeamAsset) const
+{
+	check(HasAuthority());
+	
+	// 确保团队资源资产不为空
+	ensure(TeamAsset != nullptr);
+	const UYcTeamSubsystem& TeamSubsystem = UYcTeamSubsystem::Get(this);
+	// 确保团队ID不重复
+	ensureMsgf(!TeamSubsystem.DoesTeamExist(TeamId), TEXT("队伍重复创建!"));
+
+	UWorld* World = GetWorld();
+	check(World);
+
+	// 配置Actor生成参数，确保总是能够生成
+	FActorSpawnParameters SpawnInfo;
+	SpawnInfo.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+
+	// 生成团队公开信息Actor
+	AYcTeamPublicInfo* NewTeamPublicInfo = World->SpawnActor<AYcTeamPublicInfo>(PublicTeamInfoClass, SpawnInfo);
+	checkf(NewTeamPublicInfo != nullptr, TEXT("Failed to create public team actor from class %s"), *GetPathNameSafe(*PublicTeamInfoClass));
+	// 设置团队ID和资源资产，这会触发自动注册到子系统
+	NewTeamPublicInfo->SetTeamId(TeamId);
+	NewTeamPublicInfo->SetTeamAsset(TeamAsset);
+	
+	// @TODO 创建队伍私有信息对象
+}
+
+void UYcTeamCreationComponent::OnPlayerInitialized(AGameModeBase* GameMode, const AController* NewPlayer)
+{
+	check(NewPlayer);
+	check(NewPlayer->PlayerState);
+	// 为新玩家选择并分配团队
+	ServerChooseTeamForPlayer(NewPlayer->PlayerState);
+}
+
+int32 UYcTeamCreationComponent::GetLeastPopulatedTeamID() const
+{
+	// @TODO 返回玩家人数最少的那一方队伍，以实现团队平衡
+	return 1;
+}
+#endif	// WITH_SERVER_CODE
---
+++ Plugins/YiChenGameCore/Source/YiChenTeams/Public/YcTeamCreationComponent.h
@@ -0,0 +1,101 @@
+﻿// Copyright (c) 2025 YiChen. All Rights Reserved.
+
+#pragma once
+
+#include "Components/GameStateComponent.h"
+#include "GameplayMessage/ScopedGameplayMessageListener.h"
+#include "YcTeamCreationComponent.generated.h"
+
+struct FGameplayTag;
+struct FExperienceLoadedMessage;
+class UYcTeamAsset;
+class AYcTeamPublicInfo;
+class IYcTeamAgentInterface;
+
+/**
+ * 挂载在GameState上的队伍创建及分配组件,目前的分配机制是以平衡双方人数来进行自动分配
+ * 可以配置TeamsToCreate来指定游戏对局要创建的队伍信息,支持创建多个不同队伍
+ */
+UCLASS(ClassGroup=(YiChenTeams), meta=(BlueprintSpawnableComponent))
+class YICHENTEAMS_API UYcTeamCreationComponent : public UGameStateComponent
+{
+	GENERATED_BODY()
+public:
+	UYcTeamCreationComponent(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+	//~UObject interface
+#if WITH_EDITOR
+	/**
+	 * 验证数据有效性
+	 * @param Context 数据验证上下文
+	 * @return 数据验证结果
+	 */
+	virtual EDataValidationResult IsDataValid(class FDataValidationContext& Context) const override;
+#endif
+	//~End of UObject interface
+
+	//~UActorComponent interface
+	virtual void BeginPlay() override;
+	//~End of UActorComponent interface
+
+private:
+	/**
+	 * Experience加载完毕后的回调
+	 * 将在这里创建队伍并为当前已经在对局中的玩家分配队伍ID
+	 * @param Channel 消息通道
+	 * @param Message 经验加载消息
+	 */
+	void OnExperienceLoaded(FGameplayTag Channel, const FExperienceLoadedMessage& Message);
+	
+protected:
+	/** 要创建的团队列表（团队ID到显示资源的映射，显示资源可以留空） */
+	UPROPERTY(EditDefaultsOnly, Category = Teams)
+	TMap<uint8, TObjectPtr<UYcTeamAsset>> TeamsToCreate;
+	
+	/** 队伍公开信息类 */
+	UPROPERTY(EditDefaultsOnly, Category=Teams)
+	TSubclassOf<AYcTeamPublicInfo> PublicTeamInfoClass;
+	
+#if WITH_SERVER_CODE
+protected:
+	/**
+	 * 在服务器端创建所有配置的队伍
+	 */
+	virtual void ServerCreateTeams();
+	
+	/**
+	 * 在服务器端为当前已加入对局的玩家分配TeamId
+	 */
+	virtual void ServerAssignPlayersToTeams();
+
+	/**
+	 * 为指定的玩家状态设置团队ID
+	 * 如果是观众将会被分配为FGenericTeamId::NoTeam，正常玩家则调用GetLeastPopulatedTeamID()获取一个TeamId分配给玩家
+	 * 可以通过子组件重写这个函数定制玩家的队伍分配逻辑
+	 * @param PS 要分配团队的玩家状态
+	 */
+	virtual void ServerChooseTeamForPlayer(APlayerState* PS);
+
+private:
+	/**
+	 * 在服务器端创建单个团队
+	 * @param TeamId 要创建的团队ID
+	 * @param TeamAsset 团队资源资产
+	 */
+	void ServerCreateTeam(int32 TeamId, UYcTeamAsset* TeamAsset) const;
+	
+	/**
+	 * 新玩家加入时的回调
+	 * 由GameMode发起，从这里开始为新玩家分配队伍ID
+	 * @param GameMode 游戏模式
+	 * @param NewPlayer 新加入的玩家控制器
+	 */
+	void OnPlayerInitialized(AGameModeBase* GameMode, const AController* NewPlayer);
+	
+	/**
+	 * 返回玩家数量最少的团队ID
+	 * @return 玩家最少的TeamId，如果没有有效的Team则返回INDEX_NONE
+	 */
+	int32 GetLeastPopulatedTeamID() const;
+#endif
+};

2d4b1a6e05175fbb56cfe2eb90cf517d7a4cb9bd
yichen6 <17772337761@163.com>
2025-12-22 13:28:50 +0800

    feat(YiChenGameCore): 添加GameplayTag配置文件扫描路径
    在模块启动时添加对YiChenGameCore插件Config/Tags目录下GameplayTag配置文件的扫描路径

--- Plugins/YiChenGameCore/Source/YiChenGameCore/Private/YiChenGameCore.cpp
+++ Plugins/YiChenGameCore/Source/YiChenGameCore/Private/YiChenGameCore.cpp
@@ -2,6 +2,8 @@
 
 #include "YiChenGameCore.h"
 
+#include "GameplayTagsManager.h"
+
 #define LOCTEXT_NAMESPACE "FYiChenGameCoreModule"
 
 DEFINE_LOG_CATEGORY(LogYcGameCore);
@@ -9,6 +11,8 @@ DEFINE_LOG_CATEGORY(LogYcGameCore);
 void FYiChenGameCoreModule::StartupModule()
 {
 	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+	// 扫描插件Config/Tags目录下的GameplayTag配置文件
+	UGameplayTagsManager::Get().AddTagIniSearchPath(FPaths::ProjectPluginsDir() / TEXT("YiChenGameCore/Config/Tags"));
 }
 
 void FYiChenGameCoreModule::ShutdownModule()

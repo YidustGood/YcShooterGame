6fafd7b811b45da599f26798fdd2262aecb0e15c
yichen6 <17772337761@163.com>
2025-12-22 15:58:16 +0800

    feat(团队系统): 添加团队组件实现基础团队功能
    实现团队组件用于管理玩家团队和小队信息，支持网络复制和服务器端修改
    组件提供团队ID和小队ID的获取/设置接口，并包含团队变更事件委托
    挂载到PlayerState即可接入团队系统

---
+++ Plugins/YiChenGameCore/Source/YiChenTeams/Private/YcTeamComponent.cpp
@@ -0,0 +1,96 @@
+﻿// Copyright (c) 2025 YiChen. All Rights Reserved.
+
+
+#include "YcTeamComponent.h"
+
+#include "YiChenTeams.h"
+#include "Net/UnrealNetwork.h"
+#include "Net/Core/PushModel/PushModel.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(YcTeamComponent)
+
+UYcTeamComponent::UYcTeamComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+	SetIsReplicated(true);
+}
+
+void UYcTeamComponent::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	// 配置基于推送的网络复制参数
+	FDoRepLifetimeParams SharedParams;
+	SharedParams.bIsPushBased = true;
+	// 注册团队ID和小队ID的网络复制
+	DOREPLIFETIME_WITH_PARAMS_FAST(ThisClass, MyTeamID, SharedParams);
+	DOREPLIFETIME_WITH_PARAMS_FAST(ThisClass, MySquadID, SharedParams);
+}
+
+void UYcTeamComponent::SetGenericTeamId(const FGenericTeamId& NewTeamID)
+{
+	// 仅在服务器端允许设置团队ID
+	if (GetOwner() && GetOwner()->HasAuthority())
+	{
+		const FGenericTeamId OldTeamID = MyTeamID;
+
+		// 标记属性为脏，触发网络复制
+		MARK_PROPERTY_DIRTY_FROM_NAME(ThisClass, MyTeamID, this);
+		MyTeamID = NewTeamID;
+		// 条件性地广播团队变更事件
+		ConditionalBroadcastTeamChanged(this, OldTeamID, NewTeamID);
+	}
+	else
+	{
+		UE_LOG(LogYcTeamSystem, Error, TEXT("Cannot set team for %s on non-authority"), *GetPathName(this));
+	}
+}
+
+FGenericTeamId UYcTeamComponent::GetGenericTeamId() const
+{
+	return MyTeamID;
+}
+
+FOnYcTeamIndexChangedDelegate* UYcTeamComponent::GetOnTeamIndexChangedDelegate()
+{
+	return &OnTeamChangedDelegate;
+}
+
+int32 UYcTeamComponent::GetTeamId() const
+{
+	// 将通用团队ID转换为整数形式
+	return GenericTeamIdToInteger(MyTeamID);
+}
+
+int32 UYcTeamComponent::GetSquadId() const
+{
+	return MySquadID;
+}
+
+int32 UYcTeamComponent::K2_GetTeamId() const
+{
+	return GenericTeamIdToInteger(MyTeamID);
+}
+
+void UYcTeamComponent::SetSquadID(int32 NewSquadID)
+{
+	// 仅在服务器端允许设置小队ID
+	if (GetOwner() && GetOwner()->HasAuthority())
+	{
+		// 标记属性为脏，触发网络复制
+		MARK_PROPERTY_DIRTY_FROM_NAME(ThisClass, MySquadID, this);
+
+		MySquadID = NewSquadID;
+	}
+}
+
+void UYcTeamComponent::OnRep_MyTeamID(FGenericTeamId OldTeamID)
+{
+	// 当团队ID通过网络复制到客户端时，可以在这里处理相关的客户端逻辑
+	ConditionalBroadcastTeamChanged(this, OldTeamID, MyTeamID);
+}
+
+void UYcTeamComponent::OnRep_MySquadID(int32 OldSquadId)
+{
+	// 当小队ID通过网络复制到客户端时，可以在这里处理相关的客户端逻辑
+	// 例如创建小队子系统来处理团队中的小队
+}
---
+++ Plugins/YiChenGameCore/Source/YiChenTeams/Public/YcTeamComponent.h
@@ -0,0 +1,100 @@
+﻿// Copyright (c) 2025 YiChen. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "YcTeamAgentInterface.h"
+#include "Components/ActorComponent.h"
+#include "YcTeamComponent.generated.h"
+
+/**
+ * 团队组件, 一般挂载给PlayerState, 这样玩家就拥有了团队系统所需的功能
+ * 这个组件提供常规的团队接口实现, 能够满足基本的团队功能需求, 如果需要定制更复杂的团队功能可以通过直接实现IYcTeamAgentInterface接口进行定制
+ */
+UCLASS(ClassGroup=(YiChenTeams), meta=(BlueprintSpawnableComponent))
+class YICHENTEAMS_API UYcTeamComponent : public UActorComponent, public IYcTeamAgentInterface
+{
+	GENERATED_BODY()
+public:
+	UYcTeamComponent();
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	
+	//~IYcTeamAgentInterface interface
+	/**
+	 * 设置玩家的通用团队ID
+	 * 注意：此函数只能在服务器端调用
+	 * @param NewTeamID 新的团队ID
+	 */
+	virtual void SetGenericTeamId(const FGenericTeamId& NewTeamID) override;
+	
+	/**
+	 * 获取玩家的通用团队ID
+	 * @return 当前玩家的通用团队ID
+	 */
+	virtual FGenericTeamId GetGenericTeamId() const override;
+	
+	/**
+	 * 获取团队索引变更委托的指针
+	 * @return 团队索引变更委托的指针
+	 */
+	virtual FOnYcTeamIndexChangedDelegate* GetOnTeamIndexChangedDelegate() override;
+	
+	/**
+	 * 获取玩家的团队ID（整数形式）
+	 * @return 团队ID的整数值，如果未分配团队则返回INDEX_NONE
+	 */
+	virtual int32 GetTeamId() const override;
+	//~End of IYcCTeamAgentInterface interface
+	
+	/**
+	 * 获取玩家所属的小队ID
+	 * @return 小队ID的整数值
+	 */
+	UFUNCTION(BlueprintCallable)
+	int32 GetSquadId() const;
+
+	/**
+	 * 获取玩家所属的团队ID（蓝图版本）
+	 * @return 团队ID的整数值，如果未分配团队则返回INDEX_NONE
+	 */
+	UFUNCTION(BlueprintCallable, DisplayName = "GetTeamId")
+	int32 K2_GetTeamId() const;
+
+	/**
+	 * 设置玩家的小队ID
+	 * 注意：此函数只能在服务器端调用
+	 * @param NewSquadID 新的小队ID
+	 */
+	UFUNCTION(BlueprintCallable)
+	void SetSquadID(int32 NewSquadID);
+	
+protected:
+	/** 团队ID复制属性变更时的回调函数 */
+	UFUNCTION()
+	void OnRep_MyTeamID(FGenericTeamId OldTeamID);
+	
+	/** 小队ID复制属性变更时的回调函数 */
+	UFUNCTION()
+	void OnRep_MySquadID(int32 OldSquadId);
+	
+private:
+	/** 
+	 * 常规的队伍ID，这是游戏中作为主导的TeamId
+	 * PlayerController、Character等类中获取TeamId也是转发到PlayerState中来获取TeamId
+	 * 使用基于推送的网络复制
+	 */
+	UPROPERTY(VisibleInstanceOnly, ReplicatedUsing=OnRep_MyTeamID)
+	FGenericTeamId MyTeamID;
+
+	/** 
+	 * 小队ID，如果需要的话一个队伍中可以分为多个小队
+	 * 参考战地大战场模式，盟军就是大队伍，小队则是盟军的一个子队
+	 * 使用基于推送的网络复制
+	 */
+	UPROPERTY(VisibleInstanceOnly, ReplicatedUsing=OnRep_MySquadID)
+	int32 MySquadID;
+	
+	/** 团队索引变更委托，当玩家的团队ID发生变化时触发 */
+	UPROPERTY(BlueprintAssignable)
+	FOnYcTeamIndexChangedDelegate OnTeamChangedDelegate;
+};

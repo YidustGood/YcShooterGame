84a719294ff71e85fa73f4b5ecde4df832b6930d
yichen6 <17772337761@163.com>
2025-12-22 16:41:30 +0800

    feat(团队系统): 添加观察Acotr所属团队信息变更的异步操作
    实现 UAsyncAction_ObserveTeam 类用于监听实现了 IYcTeamAgentInterface 接口的对象的团队变更。该操作会立即触发一次回调提供当前团队状态，并在团队变更时继续通知。通过弱引用管理观察对象生命周期，并在销毁时自动解除委托绑定。

---
+++ Plugins/YiChenGameCore/Source/YiChenTeams/Private/AsyncAction_ObserveTeam.cpp
@@ -0,0 +1,73 @@
+﻿// Copyright (c) 2025 YiChen. All Rights Reserved.
+
+
+#include "AsyncAction_ObserveTeam.h"
+
+#include "YcTeamAgentInterface.h"
+#include "YcTeamSubsystem.h"
+
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(AsyncAction_ObserveTeam)
+
+UAsyncAction_ObserveTeam::UAsyncAction_ObserveTeam(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
+
+UAsyncAction_ObserveTeam* UAsyncAction_ObserveTeam::ObserveTeam(AActor* TeamActor)
+{
+	// 尝试从Actor中查找团队代理接口
+	TScriptInterface<IYcTeamAgentInterface> TeamAgent;
+	if (UYcTeamSubsystem::FindTeamAgentFromActor(TeamActor, TeamAgent))
+	{
+		// 创建异步操作对象并设置团队接口引用
+		UAsyncAction_ObserveTeam* Action  = NewObject<UAsyncAction_ObserveTeam>();
+		Action->TeamInterfacePtr = TeamAgent;
+		// 注册到游戏实例，确保生命周期管理
+		Action->RegisterWithGameInstance(TeamAgent.GetObject());
+		return Action;
+	}
+	return nullptr;
+}
+
+void UAsyncAction_ObserveTeam::Activate()
+{
+	bool bCouldSucceed = false;
+	int32 CurrentTeamIndex = INDEX_NONE;
+
+	// 如果团队接口仍然有效，获取当前团队ID并绑定变更委托
+	if (IYcTeamAgentInterface* TeamInterface = TeamInterfacePtr.Get())
+	{
+		CurrentTeamIndex = GenericTeamIdToInteger(TeamInterface->GetGenericTeamId());
+		// 绑定队伍更改委托回调，以便接收未来的团队变更通知
+		TeamInterface->GetTeamChangedDelegateChecked().AddDynamic(this, &ThisClass::OnWatchedAgentChangedTeam);
+
+		bCouldSucceed = true;
+	}
+
+	// 立即广播一次，让用户获得当前状态
+	OnTeamChanged.Broadcast(CurrentTeamIndex != INDEX_NONE, CurrentTeamIndex);
+
+	// 如果无法绑定到委托，则无法获得任何后续更新，标记为准备销毁
+	if (!bCouldSucceed)
+	{
+		SetReadyToDestroy();
+	}
+}
+
+void UAsyncAction_ObserveTeam::OnWatchedAgentChangedTeam(UObject* TeamAgent, int32 OldTeam, int32 NewTeam)
+{
+	// 当被观察的团队代理变更团队时，广播变更事件
+	OnTeamChanged.Broadcast(NewTeam != INDEX_NONE, NewTeam);
+}
+
+void UAsyncAction_ObserveTeam::SetReadyToDestroy()
+{
+	Super::SetReadyToDestroy();
+
+	// 如果正在被取消，需要解除所有可能正在监听的委托绑定
+	if (IYcTeamAgentInterface* TeamInterface = TeamInterfacePtr.Get())
+	{
+		TeamInterface->GetTeamChangedDelegateChecked().RemoveAll(this);
+	}
+}
---
+++ Plugins/YiChenGameCore/Source/YiChenTeams/Public/AsyncAction_ObserveTeam.h
@@ -0,0 +1,51 @@
+﻿// Copyright (c) 2025 YiChen. All Rights Reserved.
+
+#pragma once
+
+#include "Engine/CancellableAsyncAction.h"
+#include "AsyncAction_ObserveTeam.generated.h"
+
+class IYcTeamAgentInterface;
+
+/**
+ * 观察指定对象的团队变更的异步操作
+ * 用于监听Actor的团队归属变化
+ */
+UCLASS()
+class YICHENTEAMS_API UAsyncAction_ObserveTeam : public UCancellableAsyncAction
+{
+	GENERATED_BODY()
+public:
+	UAsyncAction_ObserveTeam(const FObjectInitializer& ObjectInitializer);
+	
+	/**
+	 * 观察指定团队代理的团队变更
+	 * - 会立即触发一次以提供当前的团队分配状态
+	 * - 对于任何可以实现团队归属的对象（实现了IYcTeamAgentInterface），
+	 *   它也会监听未来的团队分配变更
+	 * @param TeamActor 要观察的团队Actor
+	 * @return 异步操作对象，如果Actor不包含团队代理接口则返回nullptr
+	 */
+	UFUNCTION(BlueprintCallable, meta=(BlueprintInternalUseOnly="true", Keywords="Watch"))
+	static UAsyncAction_ObserveTeam* ObserveTeam(AActor* TeamActor);
+	
+	//~UBlueprintAsyncActionBase interface
+	virtual void Activate() override;
+	virtual void SetReadyToDestroy() override;
+	//~End of UBlueprintAsyncActionBase interface
+	
+	/** 团队观察异步委托，当团队设置或变更时触发 */
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FTeamObservedAsyncDelegate, bool, bTeamSet, int32, TeamId);
+	
+	/** 当团队被设置或变更时调用 */
+	UPROPERTY(BlueprintAssignable)
+	FTeamObservedAsyncDelegate OnTeamChanged;
+
+private:
+	/** 被观察的团队代理变更团队时的回调函数 */
+	UFUNCTION()
+	void OnWatchedAgentChangedTeam(UObject* TeamAgent, int32 OldTeam, int32 NewTeam);
+
+	/** 弱引用指针，指向被观察的团队代理接口 */
+	TWeakInterfacePtr<IYcTeamAgentInterface> TeamInterfacePtr;
+};

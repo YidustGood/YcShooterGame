a7cc64507724a5f1498ff6667ff2493dcb3848d1
yichen6 <17772337761@163.com>
2025-12-22 14:09:41 +0800

    feat(团队系统): 添加团队代理接口
    实现IYcTeamAgentInterface接口，提供团队ID转换和变更广播功能。新增条件性广播团队变更的方法，当团队ID实际变化时触发委托通知订阅者。包含团队ID与整型的相互转换工具函数。

---
+++ Plugins/YiChenGameCore/Source/YiChenTeams/Private/YcTeamAgentInterface.cpp
@@ -0,0 +1,31 @@
+﻿// Copyright (c) 2025 YiChen. All Rights Reserved.
+
+
+#include "YcTeamAgentInterface.h"
+
+#include "YiChenTeams.h"
+#include "YiChenGameCore/Public/Utils/CommonSimpleUtil.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(YcTeamAgentInterface)
+
+UYcTeamAgentInterface::UYcTeamAgentInterface(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
+
+void IYcTeamAgentInterface::ConditionalBroadcastTeamChanged(const TScriptInterface<IYcTeamAgentInterface>& This, const FGenericTeamId OldTeamID, const FGenericTeamId NewTeamID)
+{
+	// 仅在团队ID实际发生变化时进行广播
+	if (OldTeamID != NewTeamID)
+	{
+		// 将通用团队ID转换为整数索引
+		const int32 OldTeamIndex = GenericTeamIdToInteger(OldTeamID); 
+		const int32 NewTeamIndex = GenericTeamIdToInteger(NewTeamID);
+
+		UObject* ThisObj = This.GetObject();
+		UE_LOG(LogYcTeamSystem, Verbose, TEXT("[%s] %s assigned team %d"), *GetClientServerContextString(ThisObj), *GetPathNameSafe(ThisObj), NewTeamIndex);
+
+		// 广播团队变更事件，通知所有订阅者
+		This.GetInterface()->GetTeamChangedDelegateChecked().Broadcast(ThisObj, OldTeamIndex, NewTeamIndex);
+	}
+}
---
+++ Plugins/YiChenGameCore/Source/YiChenTeams/Public/YcTeamAgentInterface.h
@@ -0,0 +1,66 @@
+﻿// Copyright (c) 2025 YiChen. All Rights Reserved.
+
+#pragma once
+
+#include "GenericTeamAgentInterface.h"
+#include "YcTeamAgentInterface.generated.h"
+
+template <typename InterfaceType> class TScriptInterface;
+
+/** 团队索引变更委托，当对象的团队ID发生变化时触发 */
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FOnYcTeamIndexChangedDelegate, UObject*, ObjectChangingTeam, int32, OldTeamID, int32, NewTeamID);
+
+/** 将通用团队ID转换为整数类型 */
+inline int32 GenericTeamIdToInteger(const FGenericTeamId ID)
+{
+	return (ID == FGenericTeamId::NoTeam) ? INDEX_NONE : static_cast<int32>(ID);
+}
+
+/** 将整数类型转换为通用团队ID */
+inline FGenericTeamId IntegerToGenericTeamId(const int32 ID)
+{
+	return (ID == INDEX_NONE) ? FGenericTeamId::NoTeam : FGenericTeamId(static_cast<uint8>(ID));
+}
+
+/** 用于与团队相关联的Actor的接口, 通过实现该接口赋予Actor团队相关的功能 */
+UINTERFACE(meta=(CannotImplementInterfaceInBlueprint))
+class UYcTeamAgentInterface : public UGenericTeamAgentInterface
+{
+	GENERATED_UINTERFACE_BODY()
+};
+
+/** 用于与团队相关联的Actor的接口, 通过实现该接口赋予Actor团队相关的功能 */
+class YICHENTEAMS_API IYcTeamAgentInterface: public IGenericTeamAgentInterface
+{
+	GENERATED_IINTERFACE_BODY()
+
+public:
+	/**
+	 * 获取当前对象的团队ID
+	 * @return 团队ID的整数值，如果未分配团队则返回INDEX_NONE
+	 */
+	virtual int32 GetTeamId() const { return GenericTeamIdToInteger(FGenericTeamId::NoTeam); };
+	
+	/**
+	 * 获取团队索引变更委托的指针
+	 * @return 团队索引变更委托的指针，如果未实现则返回nullptr
+	 */
+	virtual FOnYcTeamIndexChangedDelegate* GetOnTeamIndexChangedDelegate() { return nullptr; }
+
+	/**
+	 * 条件性地广播团队变更事件
+	 * 当旧团队ID与新团队ID不同时，触发团队变更委托
+	 * @param This 实现了IYcTeamAgentInterface接口的对象
+	 * @param OldTeamID 旧的团队ID
+	 * @param NewTeamID 新的团队ID
+	 */
+	static void ConditionalBroadcastTeamChanged(const TScriptInterface<IYcTeamAgentInterface>& This, FGenericTeamId OldTeamID, FGenericTeamId NewTeamID);
+	
+	/** 获取团队变更委托的引用（已检查非空） */
+	FOnYcTeamIndexChangedDelegate& GetTeamChangedDelegateChecked()
+	{
+		FOnYcTeamIndexChangedDelegate* Result = GetOnTeamIndexChangedDelegate();
+		check(Result);
+		return *Result;
+	}
+};

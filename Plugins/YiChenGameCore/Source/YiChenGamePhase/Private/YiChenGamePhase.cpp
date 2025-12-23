fd3b0076219e776629a9050510a45b9ae932e772
yichen6 <17772337761@163.com>
2025-12-23 22:11:24 +0800

    feat(游戏阶段): 添加YiChenGamePhase模块基础框架
    添加游戏阶段模块的基础代码，包括头文件、实现文件和构建配置
    包含模块的启动和关闭方法，以及必要的依赖项配置

---
+++ Plugins/YiChenGameCore/Source/YiChenGamePhase/Private/YiChenGamePhase.cpp
@@ -0,0 +1,19 @@
+﻿#include "YiChenGamePhase.h"
+
+#define LOCTEXT_NAMESPACE "FYiChenGamePhaseModule"
+
+DEFINE_LOG_CATEGORY(LogYcGamePhase);
+
+void FYiChenGamePhaseModule::StartupModule()
+{
+    
+}
+
+void FYiChenGamePhaseModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FYiChenGamePhaseModule, YiChenGamePhase)
---
+++ Plugins/YiChenGameCore/Source/YiChenGamePhase/YiChenGamePhase.Build.cs
@@ -0,0 +1,29 @@
+﻿using UnrealBuildTool;
+
+public class YiChenGamePhase : ModuleRules
+{
+    public YiChenGamePhase(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore",
+                "YiChenAbility",
+                "GameplayAbilities",
+                "GameplayTags"
+            }
+        );
+    }
+}

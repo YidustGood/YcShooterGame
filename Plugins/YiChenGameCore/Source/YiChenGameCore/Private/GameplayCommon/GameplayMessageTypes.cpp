23c3c7d7a2f1f59502ab328a1bcec79fb25fc99e
yichen6 <17772337761@163.com>
2025-12-26 21:26:35 +0800

    feat(游戏消息): 添加玩家初始化和ASC组件生命周期消息类型
    添加新的游戏消息类型定义，包括玩家初始化完成事件和ASC组件的初始化/取消初始化事件消息。这些消息类型将用于通过GameplayMessageSubsystem实现跨模块的低耦合通信，包括：
    1. 玩家控制器初始化完成通知
    2. 技能系统组件生命周期状态变更通知
    3. 完善相关结构体的注释和文档说明

--- Plugins/YiChenGameCore/Source/YiChenGameCore/Private/GameplayCommon/GameplayMessageTypes.cpp
+++ Plugins/YiChenGameCore/Source/YiChenGameCore/Private/GameplayCommon/GameplayMessageTypes.cpp
@@ -8,5 +8,12 @@
 
 namespace YcGameplayTags
 {
+	// 定义玩家初始化完成事件消息通道标签
 	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Gameplay_GameMode_PlayerInitialized, "Gameplay_GameMode_PlayerInitialized", "玩家登入游戏Controller初始化完成事件消息通道");
+	
+	// 定义ASC组件初始化完成消息标签
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_Ability_State_AbilitySystemInitialized, "Ability.State.AbilitySystemInitialized", "ASC组件初始化完成消息");
+	
+	// 定义ASC组件取消初始化消息标签
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(TAG_Ability_State_AbilitySystemUninitialized, "Ability.State.AbilitySystemUninitialized", "ASC组件取消初始化消息");
 }
--- Plugins/YiChenGameCore/Source/YiChenGameCore/Public/GameplayCommon/GameplayMessageTypes.h
+++ Plugins/YiChenGameCore/Source/YiChenGameCore/Public/GameplayCommon/GameplayMessageTypes.h
@@ -5,43 +5,79 @@
 #include "NativeGameplayTags.h"
 #include "GameplayMessageTypes.generated.h"
 
+/**
+ * 插件的游戏标签命名空间
+ * 用于定义和声明项目中使用的各种游戏标签
+ */
 namespace YcGameplayTags
 {
+	/** 玩家登入游戏Controller初始化完成事件消息通道标签 */
 	YICHENGAMECORE_API	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Gameplay_GameMode_PlayerInitialized);
+	
+	/** ASC组件初始化完成消息标签 */
+	YICHENGAMECORE_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_Ability_State_AbilitySystemInitialized);
+	
+	/** ASC组件取消初始化消息标签 */
+	YICHENGAMECORE_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_Ability_State_AbilitySystemUninitialized);
 }
 
 /**
- * 玩家登录后事件消息, 当玩家或机器人加入游戏时触, 以及在无缝和非无缝旅行后触发, 此时Controller已经初始化完成了
- * 配合GameplayMessageSubsystem广播消息, 实现跨模块低耦合的响应玩家加入事件
- * 是从GameMode的GenericPlayerInitialization()中广播消息, 由PostLogin()和HandleSeamlessTravelPlayer()发起的
+ * 玩家初始化完成事件消息
+ * 当玩家或机器人加入游戏时触发，以及在无缝和非无缝旅行后触发，此时Controller已经初始化完成
+ * 配合GameplayMessageSubsystem广播消息，实现跨模块低耦合的响应玩家加入事件
+ * 消息从GameMode的GenericPlayerInitialization()中广播，由PostLogin()和HandleSeamlessTravelPlayer()发起
  */
 USTRUCT(BlueprintType)
 struct FGameModePlayerInitializedMessage
 {
 	GENERATED_BODY()
+	
+	/** 默认构造函数 */
 	FGameModePlayerInitializedMessage() = default;
+	
+	/**
+	 * 带参数的构造函数
+	 * @param InGameMode 游戏模式对象
+	 * @param InNewPlayer 新加入的控制器
+	 */
 	FGameModePlayerInitializedMessage(const TObjectPtr<AGameModeBase> InGameMode, const TObjectPtr<AController> InNewPlayer):
 	GameMode(InGameMode), NewPlayer(InNewPlayer){}
 	
-	/** GameMode对象 */
+	/** 游戏模式对象 */
 	UPROPERTY(BlueprintReadOnly)
 	TObjectPtr<AGameModeBase> GameMode;
 	
-	/** 新的控制器, 可能是AI也可能是真实玩家 */
+	/** 新加入的控制器，可能是AI控制器也可能是真实玩家控制器 */
 	UPROPERTY(BlueprintReadOnly)
 	TObjectPtr<AController> NewPlayer;
 };
 
 /**
- * 游戏阶段消息, 用于发布切换游戏阶段的消息,
- * YiChenGamePhase模块的UYcGamePhaseComponent组件会监听消息处理游戏阶段切换逻辑
- * 其它模块可以通过GameplayMessageSubsystem发送这个消息来与之交互, 实现模块间的解耦
+ * 游戏阶段切换消息
+ * 用于发布游戏阶段切换事件，YiChenGamePhase模块的UYcGamePhaseComponent组件会监听此消息并处理游戏阶段切换逻辑
+ * 其它模块可以通过GameplayMessageSubsystem发送此消息来触发阶段切换，实现模块间的解耦
  */
 USTRUCT(BlueprintType)
 struct FGamePhaseMessage
 {
 	GENERATED_BODY()
-	/** 游戏阶段标签 */
+	
+	/** 目标游戏阶段的标签 */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	FGameplayTag PhaseTag;
+};
+
+/**
+ * 技能系统组件生命周期消息
+ * 用于通知技能系统组件的初始化或反初始化事件
+ * 配合GameplayMessageSubsystem使用，实现跨模块监听ASC生命周期变化
+ */
+USTRUCT(BlueprintType)
+struct FAbilitySystemLifeCycleMessage
+{
+	GENERATED_BODY()
+	
+	/** 技能系统组件对象指针 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UObject* ASC;
 };

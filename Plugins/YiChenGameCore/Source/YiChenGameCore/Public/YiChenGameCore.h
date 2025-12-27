085ef515ed377610a95fbdabf00523bdabb7f933
yichen6 <17772337761@163.com>
2025-12-27 16:15:05 +0800

    feat(YiChenGameCore): 添加AngelScript支持宏定义
    - 在YiChenGameCore.h中新增YICHEN_GAMECORE_SUPPORT_ANGELSCRIPT宏定义，用于控制插件对AngelScript版本虚幻引擎的支持。
    - 提供使用说明，建议在使用AngelScript版本的虚幻引擎时启用该宏以确保脚本类正常工作。

--- Plugins/YiChenGameCore/Source/YiChenGameCore/Public/YiChenGameCore.h
+++ Plugins/YiChenGameCore/Source/YiChenGameCore/Public/YiChenGameCore.h
@@ -6,6 +6,23 @@
 
 YICHENGAMECORE_API DECLARE_LOG_CATEGORY_EXTERN(LogYcGameCore, Log, All);
 
+/**
+ * AngelScript支持控制宏
+ * 
+ * 用于控制插件是否支持AngelScript版本的虚幻引擎
+ * 此宏定义在Core模块中，可供所有依赖Core模块的功能模块使用
+ * 
+ * 使用方式：
+ *   1. 手动控制：取消注释下面的宏定义来手动启用/禁用支持
+ * 
+ * 注意：如果使用AngelScript版本的虚幻引擎，建议启用此宏以确保脚本类能正确工作
+ * 
+ * 在其他模块中使用：
+ *   #include "YiChenGameCore.h"  // 包含Core模块头文件即可使用 YICHEN_GAMECORE_SUPPORT_ANGELSCRIPT 宏
+ */
+#define YICHEN_GAMECORE_SUPPORT_ANGELSCRIPT 1  // 启用AngelScript支持
+// #define YICHEN_GAMECORE_SUPPORT_ANGELSCRIPT 0  // 禁用AngelScript支持
+
 class FYiChenGameCoreModule : public IModuleInterface
 {
 public:

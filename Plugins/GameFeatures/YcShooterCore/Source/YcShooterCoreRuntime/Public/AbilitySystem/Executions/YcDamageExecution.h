62bd34bb1f1ee7ec7ee9177eb4d287b6535722fc
yichen6 <17772337761@163.com>
2025-12-27 18:17:13 +0800

    feat(FPS伤害系统): 添加YcDamageExecution类用于伤害计算
    - 实现UYcDamageExecution类，负责计算游戏效果对健康属性的伤害，考虑距离衰减、物理材质衰减和团队伤害规则。
    - 新增静态数据结构以缓存属性捕获定义，优化性能。
    - 通过Execute_Implementation方法实现伤害计算逻辑，确保实时获取基础伤害值并应用到目标的健康属性上。

---
+++ Plugins/GameFeatures/YcShooterCore/Source/YcShooterCoreRuntime/Public/AbilitySystem/Executions/YcDamageExecution.h
@@ -0,0 +1,30 @@
+﻿// Copyright (c) 2025 YiChen. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffectExecutionCalculation.h"
+#include "YcDamageExecution.generated.h"
+
+/**
+ * 伤害执行计算
+ * 游戏效果用于对健康属性施加伤害的执行计算类
+ * 计算最终伤害值，考虑距离衰减、物理材质衰减、团队伤害规则等因素
+ */
+UCLASS()
+class YCSHOOTERCORERUNTIME_API UYcDamageExecution : public UGameplayEffectExecutionCalculation
+{
+	GENERATED_BODY()
+public:
+	UYcDamageExecution();
+
+protected:
+
+	/**
+	 * 执行伤害计算
+	 * 从CombatSet获取基础伤害，应用距离衰减、物理材质衰减、团队伤害规则等，最终输出到HealthSet的Damage属性
+	 * @param ExecutionParams 执行参数
+	 * @param OutExecutionOutput 执行输出
+	 */
+	virtual void Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams, FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const override;
+};

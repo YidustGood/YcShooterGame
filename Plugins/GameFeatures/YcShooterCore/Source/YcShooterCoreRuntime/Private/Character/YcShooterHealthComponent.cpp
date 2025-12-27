5ebd3fead2972fd79f97b1c2ef8063e61ae98b87
yichen6 <17772337761@163.com>
2025-12-27 18:27:44 +0800

    feat(FPS健康系统): 添加YcShooterHealthComponent类
    - 实现UYcShooterHealthComponent类，负责管理角色的健康值、死亡状态及相关事件。
    - 提供健康值和最大健康值的获取方法，支持健康变化的广播和死亡序列的处理。
    - 增加死亡状态的管理，支持自毁伤害和死亡事件的处理逻辑。

---
+++ Plugins/GameFeatures/YcShooterCore/Source/YcShooterCoreRuntime/Private/Character/YcShooterHealthComponent.cpp
@@ -0,0 +1,311 @@
+﻿// Copyright (c) 2025 YiChen. All Rights Reserved.
+
+
+#include "Character/YcShooterHealthComponent.h"
+#include "NativeGameplayTags.h"
+#include "YcAbilitySystemComponent.h"
+#include "YcGameplayTags.h"
+#include "YcShooterCoreRuntimeModule.h"
+#include "AbilitySystem/Attributes/YcShooterHealthSet.h"
+#include "GameFramework/GameplayMessageSubsystem.h"
+#include "GameplayCommon/YcGameVerbMessage.h"
+#include "Net/UnrealNetwork.h"
+#include "System/YcAssetManager.h"
+#include "System/YcGameData.h"
+#include "GameFramework/PlayerState.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(YcShooterHealthComponent)
+
+UE_DEFINE_GAMEPLAY_TAG_STATIC(TAG_Yc_Elimination_Message, "Yc.Elimination.Message");
+
+UYcShooterHealthComponent::UYcShooterHealthComponent(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	PrimaryComponentTick.bStartWithTickEnabled = false;
+	PrimaryComponentTick.bCanEverTick = false;
+
+	SetIsReplicatedByDefault(true);
+
+	AbilitySystemComponent = nullptr;
+	DeathState = EYcDeathState::NotDead;
+	
+	HealthSet = nullptr;
+}
+
+void UYcShooterHealthComponent::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(UYcShooterHealthComponent, DeathState);
+}
+
+void UYcShooterHealthComponent::DoInitializeWithAbilitySystem(UYcAbilitySystemComponent* ASC)
+{
+	Super::DoInitializeWithAbilitySystem(ASC);
+	
+	const AActor* Owner = GetOwner();
+	check(Owner);
+	
+	// 创建健康组件所需的AttributeSet
+	HealthSet = AddAttributeSet<UYcShooterHealthSet>();
+	
+	// 验证属性集创建成功
+	if (!HealthSet)
+	{
+		UE_LOG(LogYcShooterCore, Error, TEXT("UYcShooterHealthComponent: Cannot initialize health component for owner [%s] with NULL health set on the ability system."), *GetNameSafe(Owner));
+		return;
+	}
+	
+	// 注意：如果需要获取已存在的属性集，可以使用GetAttributeSet<T>()函数
+	// 例如：HealthSet = GetAttributeSet<UYcShooterHealthSet>();
+
+	// 注册以监听属性更改
+	HealthSet->OnHealthChanged.AddUObject(this, &ThisClass::HandleHealthChanged);
+	HealthSet->OnMaxHealthChanged.AddUObject(this, &ThisClass::HandleMaxHealthChanged);
+	HealthSet->OnOutOfHealth.AddUObject(this, &ThisClass::HandleOutOfHealth);
+
+	// TEMP: 将属性重置为默认值。最终这将由电子表格驱动
+	AbilitySystemComponent->SetNumericAttributeBase(UYcShooterHealthSet::GetHealthAttribute(), HealthSet->GetMaxHealth());
+
+	ClearGameplayTags();
+
+	// 进行数值初始化广播，以便UI显示初始化值
+	OnHealthChanged.Broadcast(this, HealthSet->GetHealth(), HealthSet->GetHealth(), nullptr);
+	OnMaxHealthChanged.Broadcast(this, HealthSet->GetHealth(), HealthSet->GetHealth(), nullptr);
+}
+
+void UYcShooterHealthComponent::DoUninitializeFromAbilitySystem()
+{
+	ClearGameplayTags();
+
+	// 移除委托
+	if (HealthSet)
+	{
+		HealthSet->OnHealthChanged.RemoveAll(this);
+		HealthSet->OnMaxHealthChanged.RemoveAll(this);
+		HealthSet->OnOutOfHealth.RemoveAll(this);
+	}
+
+	// 清空属性引用
+	HealthSet = nullptr;
+	
+	Super::DoUninitializeFromAbilitySystem();
+}
+
+float UYcShooterHealthComponent::GetHealth() const
+{
+	return (HealthSet ? HealthSet->GetHealth() : 0.0f);
+}
+
+float UYcShooterHealthComponent::GetMaxHealth() const
+{
+	return (HealthSet ? HealthSet->GetMaxHealth() : 0.0f);
+}
+
+float UYcShooterHealthComponent::GetHealthNormalized() const
+{
+	if(!HealthSet) return 0.0f;
+	
+	const float Health = HealthSet->GetHealth();
+	const float MaxHealth = HealthSet->GetMaxHealth();
+
+	return ((MaxHealth > 0.0f) ? (Health / MaxHealth) : 0.0f);
+}
+
+void UYcShooterHealthComponent::K2_DamageSelfDestruct(bool bFellOutOfWorld)
+{
+	DamageSelfDestruct(bFellOutOfWorld);
+}
+
+void UYcShooterHealthComponent::StartDeath()
+{
+	if (DeathState != EYcDeathState::NotDead) return;
+
+	DeathState = EYcDeathState::DeathStarted;
+
+	if (AbilitySystemComponent)
+	{
+		// 为ASC添加死亡中的标签
+		AbilitySystemComponent->SetLooseGameplayTagCount(YcGameplayTags::Status_Death_Dying, 1);
+	}
+
+	AActor* Owner = GetOwner();
+	check(Owner);
+
+	// 广播开始死亡
+	OnDeathStarted.Broadcast(Owner);
+
+	// 让Owner立刻进行网络更新, 因为开始死亡是一个重要的事情
+	Owner->ForceNetUpdate();
+}
+
+void UYcShooterHealthComponent::FinishDeath()
+{
+	if (DeathState != EYcDeathState::DeathStarted) return;
+	
+	DeathState = EYcDeathState::DeathFinished;
+
+	if (AbilitySystemComponent)
+	{
+		AbilitySystemComponent->SetLooseGameplayTagCount(YcGameplayTags::Status_Death_Dead, 1);
+	}
+
+	AActor* Owner = GetOwner();
+	check(Owner);
+
+	OnDeathFinished.Broadcast(Owner);
+
+	Owner->ForceNetUpdate();
+}
+
+
+void UYcShooterHealthComponent::DamageSelfDestruct(bool bFellOutOfWorld)
+{
+	// 要处于未死亡状态且ASC组件有效才能进行对自我的摧毁性伤害
+	if ((DeathState != EYcDeathState::NotDead) || AbilitySystemComponent == nullptr) return;
+	
+	// 获取伤害GE类
+	const TSubclassOf<UGameplayEffect> DamageGE = UYcAssetManager::GetSubclass(UYcGameData::Get().DamageGameplayEffect_SetByCaller);
+	if (!DamageGE)
+	{
+		UE_LOG(LogYcShooterCore, Error, TEXT("UYcShooterHealthComponent: DamageSelfDestruct failed for owner [%s]. Unable to find gameplay effect [%s]."), *GetNameSafe(GetOwner()), *UYcGameData::Get().DamageGameplayEffect_SetByCaller.GetAssetName());
+		return;
+	}
+
+	FGameplayEffectSpecHandle SpecHandle = AbilitySystemComponent->MakeOutgoingSpec(DamageGE, 1.0f, AbilitySystemComponent->MakeEffectContext());
+	FGameplayEffectSpec* Spec = SpecHandle.Data.Get();
+
+	if (!Spec)
+	{
+		UE_LOG(LogYcShooterCore, Error, TEXT("UYcShooterHealthComponent: DamageSelfDestruct failed for owner [%s]. Unable to make outgoing spec for [%s]."), *GetNameSafe(GetOwner()), *GetNameSafe(DamageGE));
+		return;
+	}
+
+	/** 动态添加并非源自原始 GE 定义的资产标签；该标签既会被添加到动态资产标签中，也会被注入到捕获的源规格标签中 */
+	Spec->AddDynamicAssetTag(TAG_Gameplay_DamageSelfDestruct);
+
+	if (bFellOutOfWorld)
+	{
+		Spec->AddDynamicAssetTag(TAG_Gameplay_FellOutOfWorld);
+	}
+
+	// 摧毁性伤害的数值就是当前的生命值
+	const float DamageAmount = GetMaxHealth();
+
+	Spec->SetSetByCallerMagnitude(TAG_Gameplay_Attribute_SetByCaller_Damage, DamageAmount);
+	AbilitySystemComponent->ApplyGameplayEffectSpecToSelf(*Spec);
+}
+
+void UYcShooterHealthComponent::ClearGameplayTags()
+{
+	if (!AbilitySystemComponent) return;
+	
+	AbilitySystemComponent->SetLooseGameplayTagCount(YcGameplayTags::Status_Death_Dying, 0);
+	AbilitySystemComponent->SetLooseGameplayTagCount(YcGameplayTags::Status_Death_Dead, 0);
+}
+
+void UYcShooterHealthComponent::HandleHealthChanged(AActor* DamageInstigator, AActor* DamageCauser, const FGameplayEffectSpec* DamageEffectSpec, float DamageMagnitude, float OldValue, float NewValue)
+{
+	OnHealthChanged.Broadcast(this, OldValue, NewValue, DamageInstigator);
+}
+
+void UYcShooterHealthComponent::HandleMaxHealthChanged(AActor* DamageInstigator, AActor* DamageCauser, const FGameplayEffectSpec* DamageEffectSpec, float DamageMagnitude, float OldValue, float NewValue)
+{
+	OnMaxHealthChanged.Broadcast(this, OldValue, NewValue, DamageInstigator);
+}
+
+void UYcShooterHealthComponent::HandleOutOfHealth(AActor* DamageInstigator, AActor* DamageCauser, const FGameplayEffectSpec* DamageEffectSpec, float DamageMagnitude, float OldValue, float NewValue)
+{
+	// 这段代码只能在服务端执行
+#if WITH_SERVER_CODE
+	if (AbilitySystemComponent == nullptr || DamageEffectSpec == nullptr) return;
+	
+	// @TODO: 考虑到底是在这里调用StartDeath()和FinishDeath()还是在死亡技能里面调用, 两者有何区别?
+	
+	// 向ASC的AvatarActor发送"GameplayEvent.Death"事件，我们的死亡技能配置了"GameplayEvent.Death"触发器，故可触发死亡技能，然后在死亡技能里编写具体的死亡业务逻辑
+	{
+		FGameplayEventData DeathPayload;
+		DeathPayload.EventTag = YcGameplayTags::GameplayEvent_Death;
+		DeathPayload.Instigator = DamageInstigator;
+		DeathPayload.Target = AbilitySystemComponent->GetAvatarActor();
+		DeathPayload.OptionalObject = DamageEffectSpec->Def;
+		DeathPayload.ContextHandle = DamageEffectSpec->GetEffectContext();
+		DeathPayload.InstigatorTags = *DamageEffectSpec->CapturedSourceTags.GetAggregatedTags();
+		DeathPayload.TargetTags = *DamageEffectSpec->CapturedTargetTags.GetAggregatedTags();
+		DeathPayload.EventMagnitude = DamageMagnitude;
+		
+		FScopedPredictionWindow NewScopedWindow(AbilitySystemComponent, true);
+		AbilitySystemComponent->HandleGameplayEvent(DeathPayload.EventTag, &DeathPayload);
+	}
+
+	// 发送标准化的动作消息，供其他系统观察
+	// 发送玩家被消灭的消息，以便其他系统可以观察到这个事件，例如负责击杀信息的UI通过监听TAG_Yc_Elimination_Message通道的消息响应玩家死亡事件
+	{
+		FYcGameVerbMessage Message;
+		Message.Verb = TAG_Yc_Elimination_Message;
+		// 发起伤害者（ASC挂载在PlayerState的话就是PlayerState，通常是如此）
+		Message.Instigator = DamageInstigator;	
+		Message.InstigatorTags = *DamageEffectSpec->CapturedSourceTags.GetAggregatedTags();
+		// 被伤害者（ASC挂载在PlayerState的话就是PlayerState，通常是如此）
+		Message.Target = UYcGameVerbMessageHelpers::GetPlayerStateFromObject(AbilitySystemComponent->GetAvatarActor());
+		Message.TargetTags = *DamageEffectSpec->CapturedTargetTags.GetAggregatedTags();
+		//@TODO: 填充上下文标签，以及任何非技能系统的源/发起者标签
+		//@TODO: 确定是否为敌对团队击杀、自伤、团队击杀等
+
+		UGameplayMessageSubsystem& MessageSystem = UGameplayMessageSubsystem::Get(GetWorld());
+		MessageSystem.BroadcastMessage(Message.Verb, Message);
+	}
+
+	//@TODO: assist messages (could compute from damage dealt elsewhere)?
+#endif // #if WITH_SERVER_CODE
+}
+
+void UYcShooterHealthComponent::OnRep_DeathState(EYcDeathState OldDeathState)
+{
+	// 客户端的StartDeath和FinishDeath都是通过DeathState的属性同步触发
+	const EYcDeathState NewDeathState = DeathState;
+
+	// 暂时恢复死亡状态，因为我们依赖StartDeath和FinishDeath来改变它
+	DeathState = OldDeathState;
+
+	if (OldDeathState > NewDeathState)
+	{
+		// 服务器试图让我们返回，但我们已经预测超过了服务器状态
+		UE_LOG(LogYcShooterCore, Warning, TEXT("UYcShooterHealthComponent: Predicted past server death state [%d] -> [%d] for owner [%s]."), (uint8)OldDeathState, (uint8)NewDeathState, *GetNameSafe(GetOwner()));
+		return;
+	}
+
+	if (OldDeathState == EYcDeathState::NotDead)
+	{
+		// NotDead -> DeathStarted
+		if (NewDeathState == EYcDeathState::DeathStarted)
+		{
+			StartDeath();
+		}
+		// NotDead -> DeathFinished
+		else if (NewDeathState == EYcDeathState::DeathFinished)
+		{
+			StartDeath();
+			FinishDeath();
+		}
+		else
+		{
+			UE_LOG(LogYcShooterCore, Error, TEXT("UYcShooterHealthComponent: Invalid death transition [%d] -> [%d] for owner [%s]."), (uint8)OldDeathState, (uint8)NewDeathState, *GetNameSafe(GetOwner()));
+		}
+	}
+	else if (OldDeathState == EYcDeathState::DeathStarted)
+	{
+		// DeathStarted -> DeathFinished
+		if (NewDeathState == EYcDeathState::DeathFinished)
+		{
+			FinishDeath();
+		}
+		else
+		{
+			UE_LOG(LogYcShooterCore, Error, TEXT("UYcShooterHealthComponent: Invalid death transition [%d] -> [%d] for owner [%s]."), (uint8)OldDeathState, (uint8)NewDeathState, *GetNameSafe(GetOwner()));
+		}
+	}
+
+	// 检查状态处理一致性
+	ensureMsgf((DeathState == NewDeathState), TEXT("UYcShooterHealthComponent: Death transition failed [%d] -> [%d] for owner [%s]."), (uint8)OldDeathState, (uint8)NewDeathState, *GetNameSafe(GetOwner()));
+}
---
+++ Plugins/GameFeatures/YcShooterCore/Source/YcShooterCoreRuntime/Public/Character/YcShooterHealthComponent.h
@@ -0,0 +1,211 @@
+﻿// Copyright (c) 2025 YiChen. All Rights Reserved.
+
+#pragma once
+
+#include "YcAbilityComponent.h"
+
+#include "YcShooterHealthComponent.generated.h"
+
+struct FGameplayTag;
+class UYcAbilitySystemComponent;
+class UYcShooterHealthSet;
+class UYcCombatSet;
+struct FGameplayEffectSpec;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FYcShooterHealth_DeathEvent, AActor*, OwningActor);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_FourParams(FYcShooterHealth_AttributeChanged, UYcShooterHealthComponent*, HealthComponent, float, OldValue, float, NewValue, AActor*, Instigator);
+
+/**
+ *	定义当前死亡状态
+ *	用于表示当前角色的死亡状态, 我们为角色死亡定义了一个过程, 既开始和完成死亡, 以便划分出不同的逻辑阶段
+ */
+UENUM(BlueprintType)
+enum class EYcDeathState : uint8
+{
+	NotDead = 0,	// 未死亡
+	DeathStarted,	// 开始死亡
+	DeathFinished	// 完成死亡
+};
+
+/**
+ * 射击游戏健康组件
+ * 用于处理与健康相关的所有功能，包括健康值管理、死亡序列处理等
+ * 监听ASC生命周期消息进行初始化，监听健康属性变化事件，处理健康值归零事件
+ * 健康值归零后会在HandleOutOfHealth()这个回调中发送GameplayEvent触发死亡技能
+ * 然后具体的死亡逻辑一部分可以在死亡技能中实现，一部分可以在OnDeathStarted和OnDeathFinished委托的自定义回调函数中实现
+ */
+UCLASS(ClassGroup=(YiChenShooterCore), meta=(BlueprintSpawnableComponent))
+class YCSHOOTERCORERUNTIME_API UYcShooterHealthComponent : public UYcAbilityComponent
+{
+	GENERATED_BODY()
+
+public:
+	/**
+	 * 构造函数
+	 * @param ObjectInitializer 对象初始化器
+	 */
+	UYcShooterHealthComponent(const FObjectInitializer& ObjectInitializer);
+	
+	/**
+	 * 查找指定Actor上的健康组件
+	 * @param Actor 目标Actor
+	 * @return 找到的健康组件，如果不存在则返回nullptr
+	 */
+	UFUNCTION(BlueprintPure, Category = "YcGameCore|Health")
+	static UYcShooterHealthComponent* FindHealthComponent(const AActor* Actor) { return (Actor ? Actor->FindComponentByClass<UYcShooterHealthComponent>() : nullptr); }
+
+	/**
+	 * 模板方法：与AbilitySystemComponent初始化时的业务逻辑
+	 * 创建健康属性集和战斗属性集，注册属性变化委托
+	 * @param ASC 已验证的AbilitySystemComponent
+	 */
+	virtual void DoInitializeWithAbilitySystem(UYcAbilitySystemComponent* ASC) override;
+	
+	/**
+	 * 模板方法：与AbilitySystemComponent解除关联时的清理逻辑
+	 * 移除属性变化委托，清理属性集引用
+	 */
+	virtual void DoUninitializeFromAbilitySystem() override;
+	
+	/**
+	 * 获取当前健康值
+	 * @return 当前健康值
+	 */
+	UFUNCTION(BlueprintCallable, Category = "YcGameCore|Health")
+	float GetHealth() const;
+
+	/**
+	 * 获取当前最大健康值
+	 * @return 当前最大健康值
+	 */
+	UFUNCTION(BlueprintCallable, Category = "YcGameCore|Health")
+	float GetMaxHealth() const;
+	
+	/**
+	 * 获取归一化的健康值，范围[0.0, 1.0]
+	 * @return 归一化的健康值
+	 */
+	UFUNCTION(BlueprintCallable, Category = "YcGameCore|Health")
+	float GetHealthNormalized() const;
+	
+	/**
+	 * 获取当前死亡状态
+	 * @return 死亡状态
+	 */
+	UFUNCTION(BlueprintCallable, Category = "YcGameCore|Health")
+	EYcDeathState GetDeathState() const { return DeathState; }
+	
+	/**
+	 * 检查是否已死亡或正在死亡
+	 * @return 如果死亡状态大于NotDead则返回true
+	 */
+	UFUNCTION(BlueprintCallable, BlueprintPure = false, Category = "YcGameCore|Health", Meta = (ExpandBoolAsExecs = "ReturnValue"))
+	bool IsDeadOrDying() const { return (DeathState > EYcDeathState::NotDead); }
+	
+	/**
+	 * Blueprint可调用的自毁伤害函数
+	 * @param bFellOutOfWorld 是否因掉出世界而死亡
+	 */
+	UFUNCTION(BlueprintCallable, DisplayName = "DamageSelfDestruct")
+	void K2_DamageSelfDestruct(bool bFellOutOfWorld = false);
+	
+	/**
+	 * 开始死亡序列
+	 * 由服务端YcGameplayAbility_ShooterDeath技能激活时调用开始，开始后会修改DeathState状态同步到客户端，客户端根据状态进行死亡序列事件同步
+	 * 这个函数会在服务端和所有客户端执行
+	 */
+	virtual void StartDeath();
+
+	/**
+	 * 完成死亡序列
+	 * 由YcGameplayAbility_ShooterDeath技能结束后调用
+	 * 这个函数会在服务端和所有客户端执行
+	 */
+	virtual void FinishDeath();
+
+	/**
+	 * 对自身施加足以杀死Owner的伤害
+	 * @param bFellOutOfWorld 是否因掉出世界而死亡
+	 */
+	virtual void DamageSelfDestruct(bool bFellOutOfWorld = false);
+	
+	/** 
+	 * 健康值变化委托, 会在服务端和所有客户端执行, 在客户端上调用时instigator可能无效
+	 * 服务端的发起源头在UYcShooterHealthSet::PostGameplayEffectExecute()中
+	 * 客户端的发起源头在UYcShooterHealthSet::OnRep_Health()属性同步通知函数中
+	 */
+	UPROPERTY(BlueprintAssignable)
+	FYcShooterHealth_AttributeChanged OnHealthChanged;
+
+	/** 
+	 * 最大健康值变化委托, 会在服务端和所有客户端执行, 在客户端上调用时instigator可能无效
+	 * 服务端的发起源头在UYcShooterHealthSet::PostGameplayEffectExecute()中
+	 * 客户端的发起源头在UYcShooterHealthSet::OnRep_MaxHealth()属性同步通知函数中
+	 */
+	UPROPERTY(BlueprintAssignable)
+	FYcShooterHealth_AttributeChanged OnMaxHealthChanged;
+
+	/** 死亡序列开始委托, 会在服务端和所有客户端执行 */
+	UPROPERTY(BlueprintAssignable)
+	FYcShooterHealth_DeathEvent OnDeathStarted;
+
+	/** 死亡序列完成委托, 会在服务端和所有客户端执行 */
+	UPROPERTY(BlueprintAssignable)
+	FYcShooterHealth_DeathEvent OnDeathFinished;
+	
+protected:
+	/**
+	 * 清理死亡相关的GameplayTag
+	 */
+	void ClearGameplayTags();
+
+	/**
+	 * 健康值变化的回调函数, 服务端和所有客户端都会触发
+	 * @param DamageInstigator 伤害发起者
+	 * @param DamageCauser 伤害原因者
+	 * @param DamageEffectSpec 伤害效果规格
+	 * @param DamageMagnitude 伤害数值
+	 * @param OldValue 旧值
+	 * @param NewValue 新值
+	 */
+	virtual void HandleHealthChanged(AActor* DamageInstigator, AActor* DamageCauser, const FGameplayEffectSpec* DamageEffectSpec, float DamageMagnitude, float OldValue, float NewValue);
+	
+	/**
+	 * 最大健康值变化的回调函数, 服务端和所有客户端都会触发
+	 * @param DamageInstigator 伤害发起者
+	 * @param DamageCauser 伤害原因者
+	 * @param DamageEffectSpec 伤害效果规格
+	 * @param DamageMagnitude 伤害数值
+	 * @param OldValue 旧值
+	 * @param NewValue 新值
+	 */
+	virtual void HandleMaxHealthChanged(AActor* DamageInstigator, AActor* DamageCauser, const FGameplayEffectSpec* DamageEffectSpec, float DamageMagnitude, float OldValue, float NewValue);
+	
+	/**
+	 * 健康值归零的回调函数, 服务端和所有客户端都会触发
+	 * 发送死亡游戏事件触发死亡技能，发送消灭消息供其他系统观察
+	 * @param DamageInstigator 伤害发起者
+	 * @param DamageCauser 伤害原因者
+	 * @param DamageEffectSpec 伤害效果规格
+	 * @param DamageMagnitude 伤害数值
+	 * @param OldValue 旧值
+	 * @param NewValue 新值
+	 */
+	virtual void HandleOutOfHealth(AActor* DamageInstigator, AActor* DamageCauser, const FGameplayEffectSpec* DamageEffectSpec, float DamageMagnitude, float OldValue, float NewValue);
+
+	/**
+	 * 死亡状态网络复制回调
+	 * 客户端根据状态变化调用StartDeath和FinishDeath
+	 * @param OldDeathState 旧死亡状态
+	 */
+	UFUNCTION()
+	virtual void OnRep_DeathState(EYcDeathState OldDeathState);
+	
+	/** 此Actor使用的健康属性集 */
+	UPROPERTY(VisibleAnywhere, Category= "AttributeSet")
+	TObjectPtr<const UYcShooterHealthSet> HealthSet;
+	
+	/** 用于处理死亡状态的复制状态 */
+	UPROPERTY(ReplicatedUsing = OnRep_DeathState)
+	EYcDeathState DeathState;
+};

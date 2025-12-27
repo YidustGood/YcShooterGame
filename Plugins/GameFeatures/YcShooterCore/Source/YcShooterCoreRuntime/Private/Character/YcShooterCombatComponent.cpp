3e0f73b7ccb27ca7c4590fedd152c0645797dbe7
yichen6 <17772337761@163.com>
2025-12-27 17:10:39 +0800

    feat(FPS战斗组件): 添加YcShooterCombatComponent类
    - 实现了UYcShooterCombatComponent类，用于管理角色的战斗属性集，包括基础伤害和治疗属性。
    - 该组件继承自UYcAbilityComponent，自动处理与AbilitySystemComponent的生命周期管理。
    - 新增CombatSet属性集，用于存储和计算战斗相关属性，支持动态增伤机制。

---
+++ Plugins/GameFeatures/YcShooterCore/Source/YcShooterCoreRuntime/Private/Character/YcShooterCombatComponent.cpp
@@ -0,0 +1,32 @@
+﻿// Copyright (c) 2025 YiChen. All Rights Reserved.
+
+
+#include "Character/YcShooterCombatComponent.h"
+#include "AbilitySystem/Attributes/YcCombatSet.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(YcShooterCombatComponent)
+
+UYcShooterCombatComponent::UYcShooterCombatComponent(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	// 组件本身不需要Tick，仅通过属性变化和技能系统驱动逻辑
+	PrimaryComponentTick.bStartWithTickEnabled = false;
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UYcShooterCombatComponent::DoInitializeWithAbilitySystem(UYcAbilitySystemComponent* ASC)
+{
+	Super::DoInitializeWithAbilitySystem(ASC);
+	
+	// 创建战斗属性集，用于存储基础伤害、基础治疗等战斗属性
+	// 这些属性会在伤害执行（YcDamageExecution）中被使用，用于计算最终伤害值
+	CombatSet = AddAttributeSet<UYcCombatSet>();
+}
+
+void UYcShooterCombatComponent::DoUninitializeFromAbilitySystem()
+{
+	// 清理战斗属性集引用
+	CombatSet = nullptr;
+	
+	Super::DoUninitializeFromAbilitySystem();
+}

419c98148591ced1a142e5940a7efa9257e17df8
yichen6 <17772337761@163.com>
2026-01-11 01:07:23 +0800

    feat(YiChenShooterCore): 添加武器系统蓝图函数库
    - 该函数库用于提供一些便于使用武器系统的工具函数

---
+++ Plugins/YiChenShooterCore/Source/YiChenShooterCore/Private/Weapons/YcWeaponLibrary.cpp
@@ -0,0 +1,197 @@
+﻿// Copyright (c) 2025 YiChen. All Rights Reserved.
+
+
+#include "Weapons/YcWeaponLibrary.h"
+
+#include "YcEquipmentInstance.h"
+#include "Fragments/YcEquipmentFragment.h"
+#include "Weapons/YcWeaponVisualData.h"
+#include "Weapons/YcHitScanWeaponInstance.h"
+
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(YcWeaponLibrary)
+
+UYcWeaponVisualData* UYcWeaponLibrary::GetWeaponVisualData(UYcEquipmentInstance* Equipment)
+{
+	if (!Equipment)
+	{
+		return nullptr;
+	}
+
+	if (const FEquipmentFragment_VisualAsset* VisualAsset = Equipment->GetTypedFragment<FEquipmentFragment_VisualAsset>())
+	{
+		return VisualAsset->GetLoadedVisualDataAs<UYcWeaponVisualData>();
+	}
+
+	return nullptr;
+}
+
+void UYcWeaponLibrary::BreakWeaponStats(
+	const FYcComputedWeaponStats& Stats,
+	int32& BulletsPerCartridge,
+	float& MaxDamageRange,
+	float& BulletTraceSweepRadius,
+	float& FireRate,
+	float& BaseDamage,
+	float& HeadshotMultiplier,
+	float& ArmorPenetration,
+	int32& MagazineSize,
+	int32& MaxReserveAmmo,
+	float& ReloadTime,
+	float& TacticalReloadTime,
+	float& HipFireBaseSpread,
+	float& HipFireMaxSpread,
+	float& HipFireSpreadPerShot,
+	float& SpreadRecoveryRate,
+	float& SpreadRecoveryDelay,
+	float& SpreadExponent,
+	float& ADSBaseSpread,
+	float& ADSMaxSpread,
+	float& ADSSpreadPerShot,
+	float& MovingSpreadMultiplier,
+	float& JumpingSpreadMultiplier,
+	float& CrouchingSpreadMultiplier,
+	float& VerticalRecoilMin,
+	float& VerticalRecoilMax,
+	float& HorizontalRecoilMin,
+	float& HorizontalRecoilMax,
+	float& ADSRecoilMultiplier,
+	float& HipFireRecoilMultiplier,
+	float& CrouchingRecoilMultiplier,
+	float& RecoilRecoveryRate,
+	float& RecoilRecoveryDelay,
+	float& RecoilRecoveryPercent,
+	float& AimDownSightTime,
+	float& ADSFOVMultiplier,
+	float& ADSMoveSpeedMultiplier)
+{
+	BulletsPerCartridge = Stats.BulletsPerCartridge;
+	MaxDamageRange = Stats.MaxDamageRange;
+	BulletTraceSweepRadius = Stats.BulletTraceSweepRadius;
+	FireRate = Stats.FireRate;
+	BaseDamage = Stats.BaseDamage;
+	HeadshotMultiplier = Stats.HeadshotMultiplier;
+	ArmorPenetration = Stats.ArmorPenetration;
+	MagazineSize = Stats.MagazineSize;
+	MaxReserveAmmo = Stats.MaxReserveAmmo;
+	ReloadTime = Stats.ReloadTime;
+	TacticalReloadTime = Stats.TacticalReloadTime;
+	HipFireBaseSpread = Stats.HipFireBaseSpread;
+	HipFireMaxSpread = Stats.HipFireMaxSpread;
+	HipFireSpreadPerShot = Stats.HipFireSpreadPerShot;
+	SpreadRecoveryRate = Stats.SpreadRecoveryRate;
+	SpreadRecoveryDelay = Stats.SpreadRecoveryDelay;
+	SpreadExponent = Stats.SpreadExponent;
+	ADSBaseSpread = Stats.ADSBaseSpread;
+	ADSMaxSpread = Stats.ADSMaxSpread;
+	ADSSpreadPerShot = Stats.ADSSpreadPerShot;
+	MovingSpreadMultiplier = Stats.MovingSpreadMultiplier;
+	JumpingSpreadMultiplier = Stats.JumpingSpreadMultiplier;
+	CrouchingSpreadMultiplier = Stats.CrouchingSpreadMultiplier;
+	VerticalRecoilMin = Stats.VerticalRecoilMin;
+	VerticalRecoilMax = Stats.VerticalRecoilMax;
+	HorizontalRecoilMin = Stats.HorizontalRecoilMin;
+	HorizontalRecoilMax = Stats.HorizontalRecoilMax;
+	ADSRecoilMultiplier = Stats.ADSRecoilMultiplier;
+	HipFireRecoilMultiplier = Stats.HipFireRecoilMultiplier;
+	CrouchingRecoilMultiplier = Stats.CrouchingRecoilMultiplier;
+	RecoilRecoveryRate = Stats.RecoilRecoveryRate;
+	RecoilRecoveryDelay = Stats.RecoilRecoveryDelay;
+	RecoilRecoveryPercent = Stats.RecoilRecoveryPercent;
+	AimDownSightTime = Stats.AimDownSightTime;
+	ADSFOVMultiplier = Stats.ADSFOVMultiplier;
+	ADSMoveSpeedMultiplier = Stats.ADSMoveSpeedMultiplier;
+}
+
+void UYcWeaponLibrary::BreakWeaponStats_Damage(
+	const FYcComputedWeaponStats& Stats,
+	float& BaseDamage,
+	float& HeadshotMultiplier,
+	float& ArmorPenetration,
+	float& MaxDamageRange)
+{
+	BaseDamage = Stats.BaseDamage;
+	HeadshotMultiplier = Stats.HeadshotMultiplier;
+	ArmorPenetration = Stats.ArmorPenetration;
+	MaxDamageRange = Stats.MaxDamageRange;
+}
+
+void UYcWeaponLibrary::BreakWeaponStats_Firing(
+	const FYcComputedWeaponStats& Stats,
+	int32& BulletsPerCartridge,
+	float& FireRate,
+	float& MaxDamageRange,
+	float& BulletTraceSweepRadius)
+{
+	BulletsPerCartridge = Stats.BulletsPerCartridge;
+	FireRate = Stats.FireRate;
+	MaxDamageRange = Stats.MaxDamageRange;
+	BulletTraceSweepRadius = Stats.BulletTraceSweepRadius;
+}
+
+void UYcWeaponLibrary::BreakWeaponStats_Ammo(
+	const FYcComputedWeaponStats& Stats,
+	int32& MagazineSize,
+	int32& MaxReserveAmmo,
+	float& ReloadTime,
+	float& TacticalReloadTime)
+{
+	MagazineSize = Stats.MagazineSize;
+	MaxReserveAmmo = Stats.MaxReserveAmmo;
+	ReloadTime = Stats.ReloadTime;
+	TacticalReloadTime = Stats.TacticalReloadTime;
+}
+
+void UYcWeaponLibrary::BreakWeaponStats_Spread(
+	const FYcComputedWeaponStats& Stats,
+	float& HipFireBaseSpread,
+	float& HipFireMaxSpread,
+	float& HipFireSpreadPerShot,
+	float& ADSBaseSpread,
+	float& ADSMaxSpread,
+	float& ADSSpreadPerShot,
+	float& SpreadRecoveryRate,
+	float& SpreadRecoveryDelay,
+	float& SpreadExponent,
+	float& MovingSpreadMultiplier,
+	float& JumpingSpreadMultiplier,
+	float& CrouchingSpreadMultiplier)
+{
+	HipFireBaseSpread = Stats.HipFireBaseSpread;
+	HipFireMaxSpread = Stats.HipFireMaxSpread;
+	HipFireSpreadPerShot = Stats.HipFireSpreadPerShot;
+	ADSBaseSpread = Stats.ADSBaseSpread;
+	ADSMaxSpread = Stats.ADSMaxSpread;
+	ADSSpreadPerShot = Stats.ADSSpreadPerShot;
+	SpreadRecoveryRate = Stats.SpreadRecoveryRate;
+	SpreadRecoveryDelay = Stats.SpreadRecoveryDelay;
+	SpreadExponent = Stats.SpreadExponent;
+	MovingSpreadMultiplier = Stats.MovingSpreadMultiplier;
+	JumpingSpreadMultiplier = Stats.JumpingSpreadMultiplier;
+	CrouchingSpreadMultiplier = Stats.CrouchingSpreadMultiplier;
+}
+
+void UYcWeaponLibrary::BreakWeaponStats_Recoil(
+	const FYcComputedWeaponStats& Stats,
+	float& VerticalRecoilMin,
+	float& VerticalRecoilMax,
+	float& HorizontalRecoilMin,
+	float& HorizontalRecoilMax,
+	float& ADSRecoilMultiplier,
+	float& HipFireRecoilMultiplier,
+	float& CrouchingRecoilMultiplier,
+	float& RecoilRecoveryRate,
+	float& RecoilRecoveryDelay,
+	float& RecoilRecoveryPercent)
+{
+	VerticalRecoilMin = Stats.VerticalRecoilMin;
+	VerticalRecoilMax = Stats.VerticalRecoilMax;
+	HorizontalRecoilMin = Stats.HorizontalRecoilMin;
+	HorizontalRecoilMax = Stats.HorizontalRecoilMax;
+	ADSRecoilMultiplier = Stats.ADSRecoilMultiplier;
+	HipFireRecoilMultiplier = Stats.HipFireRecoilMultiplier;
+	CrouchingRecoilMultiplier = Stats.CrouchingRecoilMultiplier;
+	RecoilRecoveryRate = Stats.RecoilRecoveryRate;
+	RecoilRecoveryDelay = Stats.RecoilRecoveryDelay;
+	RecoilRecoveryPercent = Stats.RecoilRecoveryPercent;
+}
---
+++ Plugins/YiChenShooterCore/Source/YiChenShooterCore/Public/Weapons/YcWeaponLibrary.h
@@ -0,0 +1,168 @@
+﻿// Copyright (c) 2025 YiChen. All Rights Reserved.
+
+#pragma once
+
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "Fragments/YcEquipmentFragment.h"
+#include "YcWeaponLibrary.generated.h"
+
+class UYcEquipmentInstance;
+class UYcWeaponVisualData;
+struct FYcComputedWeaponStats;
+
+/**
+ * 武器系统蓝图函数库
+ * 提供武器相关的便捷函数
+ */
+UCLASS()
+class YICHENSHOOTERCORE_API UYcWeaponLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+public:
+	/**
+	 * 获取装备的武器视觉数据（便捷函数）
+	 * @param Equipment 装备实例
+	 * @return 武器视觉数据指针，未找到或未加载返回 nullptr
+	 */
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Weapon|Fragment")
+	static UYcWeaponVisualData* GetWeaponVisualData(UYcEquipmentInstance* Equipment);
+
+	// ════════════════════════════════════════════════════════════════════════
+	// Break 函数 - 分解武器数值结构体
+	// ════════════════════════════════════════════════════════════════════════
+
+	/**
+	 * 分解武器数值 - 完整版
+	 * 包含所有武器属性，未使用的引脚可以在蓝图中收起
+	 */
+	UFUNCTION(BlueprintPure, Category = "Weapon|Stats", meta = (NativeBreakFunc, AdvancedDisplay = "1"))
+	static void BreakWeaponStats(
+		const FYcComputedWeaponStats& Stats,
+		// 射击参数
+		int32& BulletsPerCartridge,
+		float& MaxDamageRange,
+		float& BulletTraceSweepRadius,
+		float& FireRate,
+		// 伤害参数
+		float& BaseDamage,
+		float& HeadshotMultiplier,
+		float& ArmorPenetration,
+		// 弹药参数
+		int32& MagazineSize,
+		int32& MaxReserveAmmo,
+		float& ReloadTime,
+		float& TacticalReloadTime,
+		// 腰射扩散参数
+		float& HipFireBaseSpread,
+		float& HipFireMaxSpread,
+		float& HipFireSpreadPerShot,
+		float& SpreadRecoveryRate,
+		float& SpreadRecoveryDelay,
+		float& SpreadExponent,
+		// 瞄准扩散参数
+		float& ADSBaseSpread,
+		float& ADSMaxSpread,
+		float& ADSSpreadPerShot,
+		// 状态扩散乘数
+		float& MovingSpreadMultiplier,
+		float& JumpingSpreadMultiplier,
+		float& CrouchingSpreadMultiplier,
+		// 后坐力参数
+		float& VerticalRecoilMin,
+		float& VerticalRecoilMax,
+		float& HorizontalRecoilMin,
+		float& HorizontalRecoilMax,
+		float& ADSRecoilMultiplier,
+		float& HipFireRecoilMultiplier,
+		float& CrouchingRecoilMultiplier,
+		float& RecoilRecoveryRate,
+		float& RecoilRecoveryDelay,
+		float& RecoilRecoveryPercent,
+		// 瞄准参数
+		float& AimDownSightTime,
+		float& ADSFOVMultiplier,
+		float& ADSMoveSpeedMultiplier
+	);
+
+	/**
+	 * 分解武器数值 - 伤害相关
+	 * 只包含伤害计算需要的属性
+	 */
+	UFUNCTION(BlueprintPure, Category = "Weapon|Stats", meta = (NativeBreakFunc))
+	static void BreakWeaponStats_Damage(
+		const FYcComputedWeaponStats& Stats,
+		float& BaseDamage,
+		float& HeadshotMultiplier,
+		float& ArmorPenetration,
+		float& MaxDamageRange
+	);
+
+	/**
+	 * 分解武器数值 - 射击相关
+	 * 只包含射击机制需要的属性
+	 */
+	UFUNCTION(BlueprintPure, Category = "Weapon|Stats", meta = (NativeBreakFunc))
+	static void BreakWeaponStats_Firing(
+		const FYcComputedWeaponStats& Stats,
+		int32& BulletsPerCartridge,
+		float& FireRate,
+		float& MaxDamageRange,
+		float& BulletTraceSweepRadius
+	);
+
+	/**
+	 * 分解武器数值 - 弹药相关
+	 */
+	UFUNCTION(BlueprintPure, Category = "Weapon|Stats", meta = (NativeBreakFunc))
+	static void BreakWeaponStats_Ammo(
+		const FYcComputedWeaponStats& Stats,
+		int32& MagazineSize,
+		int32& MaxReserveAmmo,
+		float& ReloadTime,
+		float& TacticalReloadTime
+	);
+
+	/**
+	 * 分解武器数值 - 扩散相关
+	 */
+	UFUNCTION(BlueprintPure, Category = "Weapon|Stats", meta = (NativeBreakFunc, AdvancedDisplay = "4"))
+	static void BreakWeaponStats_Spread(
+		const FYcComputedWeaponStats& Stats,
+		// 腰射
+		float& HipFireBaseSpread,
+		float& HipFireMaxSpread,
+		float& HipFireSpreadPerShot,
+		// 瞄准
+		float& ADSBaseSpread,
+		float& ADSMaxSpread,
+		float& ADSSpreadPerShot,
+		// 恢复
+		float& SpreadRecoveryRate,
+		float& SpreadRecoveryDelay,
+		float& SpreadExponent,
+		// 状态乘数
+		float& MovingSpreadMultiplier,
+		float& JumpingSpreadMultiplier,
+		float& CrouchingSpreadMultiplier
+	);
+
+	/**
+	 * 分解武器数值 - 后坐力相关
+	 */
+	UFUNCTION(BlueprintPure, Category = "Weapon|Stats", meta = (NativeBreakFunc, AdvancedDisplay = "5"))
+	static void BreakWeaponStats_Recoil(
+		const FYcComputedWeaponStats& Stats,
+		float& VerticalRecoilMin,
+		float& VerticalRecoilMax,
+		float& HorizontalRecoilMin,
+		float& HorizontalRecoilMax,
+		// 乘数
+		float& ADSRecoilMultiplier,
+		float& HipFireRecoilMultiplier,
+		float& CrouchingRecoilMultiplier,
+		// 恢复
+		float& RecoilRecoveryRate,
+		float& RecoilRecoveryDelay,
+		float& RecoilRecoveryPercent
+	);
+};

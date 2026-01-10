b13b37d2b7be0b0e6a4b80bf88086f4a1447ed0f
yichen6 <17772337761@163.com>
2026-01-10 13:28:16 +0800

    feat(YiChenShooterCore): 添加死亡技能基类
    - 创建UYcGameplayAbility_ShooterDeath死亡技能类，用于处理角色死亡逻辑
    - 实现ActivateAbility方法，取消其他技能并设置为阻塞性排他技能
    - 实现EndAbility方法，确保死亡序列完成
    - 添加StartDeath方法，调用HealthComponent开始死亡流程
    - 添加FinishDeath方法，调用HealthComponent完成死亡流程
    - 配置技能通过GameplayEvent_Death事件自动触发
    - 支持蓝图调用死亡相关方法，便于扩展死亡逻辑

---
+++ Plugins/YiChenShooterCore/Source/YiChenShooterCore/Public/AbilitySystem/Abilities/YcGameplayAbility_ShooterDeath.h
@@ -0,0 +1,66 @@
+﻿// Copyright (c) 2025 YiChen. All Rights Reserved.
+
+#pragma once
+
+#include "Abilities/YcGameplayAbility.h"
+#include "YcGameplayAbility_ShooterDeath.generated.h"
+
+/**
+ * 射击游戏死亡技能
+ * 用于处理角色死亡的技能，实际逻辑是转发到Avatar actor的HealthComponent->StartDeath以及FinishDeath进行处理
+ * 技能通过"GameplayEvent.Death"能力触发标签自动激活
+ * 玩家血量归零后UYcShooterHealthComponent::HandleOutOfHealth()中会发送一个"GameplayEvent.Death"事件来触发这个死亡技能
+ */
+UCLASS(Abstract)
+class YICHENSHOOTERCORE_API UYcGameplayAbility_ShooterDeath : public UYcGameplayAbility
+{
+	GENERATED_BODY()
+public:
+	/**
+	 * 构造函数
+	 * @param ObjectInitializer 对象初始化器
+	 */
+	UYcGameplayAbility_ShooterDeath(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+	
+protected:
+
+	/**
+	 * 激活技能
+	 * 取消其他技能，设置为阻塞性排他技能，如果启用自动开始死亡则调用StartDeath
+	 * @param Handle 技能规格句柄
+	 * @param ActorInfo Actor信息
+	 * @param ActivationInfo 激活信息
+	 * @param TriggerEventData 触发事件数据
+	 */
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+	
+	/**
+	 * 结束技能
+	 * 总是尝试完成死亡序列（如果死亡已开始）
+	 * @param Handle 技能规格句柄
+	 * @param ActorInfo Actor信息
+	 * @param ActivationInfo 激活信息
+	 * @param bReplicateEndAbility 是否复制结束技能
+	 * @param bWasCancelled 是否被取消
+	 */
+	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
+
+	/**
+	 * 开始死亡序列
+	 * 调用HealthComponent的StartDeath方法
+	 */
+	UFUNCTION(BlueprintCallable, Category = "YcGameCore|Ability")
+	void StartDeath();
+
+	/**
+	 * 完成死亡序列
+	 * 调用HealthComponent的FinishDeath方法
+	 */
+	UFUNCTION(BlueprintCallable, Category = "YcGameCore|Ability")
+	void FinishDeath();
+
+protected:
+	/** 如果启用，技能将自动调用StartDeath。如果死亡已开始，FinishDeath总是在技能结束时被调用 */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "YcGameCore|Death")
+	bool bAutoStartDeath;
+};

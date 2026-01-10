4475e9ed71e41a5ec5bcb5e17c1c4b44474f3507
yichen6 <17772337761@163.com>
2026-01-10 20:05:54 +0800

    refactor(装备系统): 调整装备视觉资产属性特性
    - 将 FEquipmentFragment_VisualAsset 中的 AssetBundleNames 属性从 EditAnywhere 改为 VisibleAnywhere，防止意外修改
    - 为 UYcEquipmentVisualData 中的 ExtendedAssets 属性添加 Categories 元数据，优化资产筛选和组织
    - 改进装备视觉配置的属性可访问性和编辑器工作流

--- Plugins/YiChenGameCore/Source/YiChenEquipment/Public/YcEquipmentVisualData.h
+++ Plugins/YiChenGameCore/Source/YiChenEquipment/Public/YcEquipmentVisualData.h
@@ -127,7 +127,7 @@ public:
 	 * - Asset.Equipment.Sound.XXX    - 音效资产
 	 * - Asset.Equipment.Material.XXX - 材质资产
 	 */
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Visual|Extended", meta = (AssetBundles = "EquipmentVisual"))
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Visual|Extended", meta = (AssetBundles = "EquipmentVisual", Categories = "Asset"))
 	TMap<FGameplayTag, FYcEquipmentVisualAssetEntry> ExtendedAssets;
 
 	// ================================================================================

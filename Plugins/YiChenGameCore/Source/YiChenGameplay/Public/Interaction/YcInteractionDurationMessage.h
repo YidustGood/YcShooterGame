6165797440f5c02d9ae88bf5c9f8cd5082a7ea6f
yichen6 <17772337761@163.com>
2026-01-31 18:48:56 +0800

    feat(Interaction): 添加交互持续时间消息结构体
    - 创建YcInteractionDurationMessage消息结构体用于传递交互持续时间信息
    - 添加TAG_INTERACTION_DURATION_MESSAGE游戏标签用于标识交互消息类型
    - 包含Instigator和Duration两个属性用于记录交互发起者和持续时长
    - 支持蓝图读写，便于各类交互系统（如重生倒计时）发送持续事件消息

---
+++ Plugins/YiChenGameCore/Source/YiChenGameplay/Public/Interaction/YcInteractionDurationMessage.h
@@ -0,0 +1,22 @@
+﻿// Copyright (c) 2025 YiChen. All Rights Reserved.
+
+#pragma once
+
+#include "NativeGameplayTags.h"
+#include "YcInteractionDurationMessage.generated.h"
+
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_INTERACTION_DURATION_MESSAGE);
+
+/** 可用于各类交互发送持续实践的消息结构体, 例如发送重生倒计时之类的 */
+USTRUCT(BlueprintType)
+struct FYcInteractionDurationMessage
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintReadWrite)
+	TObjectPtr<AActor> Instigator = nullptr;
+
+	UPROPERTY(BlueprintReadWrite)
+	float Duration = 0;
+};
